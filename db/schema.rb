9fbc7e24bce78b7551f6b12b7bb9e9d069888c41
Greg Leppert <greg@formasfunction.com>
2011-07-24 17:59:35 -0400

    Adding posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110724202332) do
+ActiveRecord::Schema.define(:version => 20110724214546) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "url"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "provider"

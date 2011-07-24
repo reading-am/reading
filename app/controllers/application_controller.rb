c88f2282d2c29024dcdbb37ab08a3a0470e7b3a5
Greg Leppert <greg@formasfunction.com>
2011-07-24 19:15:56 -0400

    Adding titles to posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
   helper_method :current_user
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def check_login
+    if current_user
+      redirect_to :controller => :posts, :action => index
+    end
   end
 end

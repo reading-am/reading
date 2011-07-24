b975bfb22dca2bf4eb365207a3d46ac71e12708f
Greg Leppert <greg@formasfunction.com>
2011-07-24 16:40:20 -0400

    Twitter omniauth working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'saMngnlSaaIapiznEqqw', '***REMOVED***'
+  provider :facebook, '241046329250415', '***REMOVED***', {:scope => 'email, offline_access'}
 end

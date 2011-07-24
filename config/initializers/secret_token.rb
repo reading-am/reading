08fe46924b409b453b98a468614c9777cecf55e1
Greg Leppert <greg@formasfunction.com>
2011-07-24 08:19:24 -0400

    Starting on reading

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reading::Application.config.secret_token = '3a0fa968d1cc7548fa923141393e9d6d6ed8fe0fc8caff5642ebe68ebfaf8f204b67eaf795803f4bf4d22e25e12ff4ce1bf17a6e54289e575692ffddac726e01'

01768b49164bbeef78a682da167f5346755cb130
Greg Leppert <greg@formasfunction.com>
2011-07-24 17:32:49 -0400

    Working on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  validates_uniqueness_of :invitation_id, :on => :create, :message => 'has already been used'
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]

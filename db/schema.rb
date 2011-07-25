28a90ec777dc7ce2575414cb8636e3951e05dec5
Greg Leppert <greg@formasfunction.com>
2011-07-24 20:11:30 -0400

    Adding a token to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110724231532) do
+ActiveRecord::Schema.define(:version => 20110725001052) do
 
   create_table "posts", :force => true do |t|
     t.string   "url"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20110724231532) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "token"
   end
 
 end

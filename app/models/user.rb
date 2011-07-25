44fc0b7583ace0b8302f2ecc9000d14956409735
Greg Leppert <greg@formasfunction.com>
2011-07-24 20:11:30 -0400

    Adding a token to the users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ActiveRecord::Base
 
   def self.create_with_omniauth(auth)
     create! do |user|
+      require "digest"
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["user_info"]["name"]
+      user.token = Digest::MD5.hexdigest(Time.new.to_s + user.uid)
     end
   end
 end

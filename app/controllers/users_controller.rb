41a7b3187e479ce2b39b6fd865a45fe8275617a4
Greg Leppert <greg@formasfunction.com>
2011-07-30 16:37:45 -0400

    Working on settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,9 @@ class UsersController < ApplicationController
     else
       @user = User.find(params[:id])
     end
+    if @user != current_user
+      redirect_to "/#{@user.username}"
+    end
   end
 
   # POST /users

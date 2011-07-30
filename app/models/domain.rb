20f744c420652196d42395dd91bc4415daf3249b
Greg Leppert <greg@formasfunction.com>
2011-07-30 13:32:58 -0400

    Domain creation is working

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,4 +1,8 @@
 class Domain < ActiveRecord::Base
   has_many :posts
   has_many :users, :through => :posts
+
+  def to_param
+    name
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_one :domain
+  belongs_to :domain
 end

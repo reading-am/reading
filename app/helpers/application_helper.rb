46b05b1251d890fe98e55712b984640c70e597eb
Greg Leppert <greg@formasfunction.com>
2011-07-30 16:28:57 -0400

    Adding a blank slate for posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def bookmarklet_url user
+    "javascript:reading={};(function(){reading.token='#{user.token}';var otherlib=(typeof jQuery=='undefined'&&typeof $=='function');function getScript(url,success){var script=document.createElement('script');script.src=url;var head=document.getElementsByTagName('head')[0],done=false;script.onload=script.onreadystatechange=function(){if(!done&&(!this.readyState||this.readyState=='loaded'||this.readyState=='complete')){done=true;success();script.onload=script.onreadystatechange=null;head.removeChild(script);}};head.appendChild(script);}getScript('http://code.jquery.com/jquery-latest.min.js',function(){if(typeof jQuery=='undefined'){alert('There was an error loading jquery');}else{jQuery.getScript('http://0.0.0.0:3000/javascripts/reading.js');}});})();"
+  end
 end

5a576b3cf69022344db115474206032af66aa0ca
Greg Leppert <greg@formasfunction.com>
2011-07-30 16:57:27 -0400

    Adding hooks

--- app/models/hook.rb
+++ app/models/hook.rb
@@ -1,3 +1,13 @@
 class Hook < ActiveRecord::Base
   belongs_to :user
+
+  def execute
+    if this.provider == 'hipchat'
+      # api_token = '***REMOVED***'
+      client = HipChat::Client.new(self.token)
+      notify_users = true
+      message = render_to_string :partial => 'posts/hipchat_message.html.erb'
+      client[self.action].send('Reading.am', "#{message}", notify_users)
+    end
+  end
 end

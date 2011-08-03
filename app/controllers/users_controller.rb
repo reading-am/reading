5e72159308013e5e73fab031806c7437f0e7a66d
Greg Leppert <greg@formasfunction.com>
2011-08-03 00:24:53 -0400

    Fixing deleting of posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -106,7 +106,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if params[:user] and current_user.update_attributes(params[:user])
-        format.html { redirect_to("/#{current_user.username}/posts", :notice => 'User was successfully updated.') }
+        format.html { redirect_to("/#{current_user.username}", :notice => 'User was successfully updated.') }
       else
         format.html
       end

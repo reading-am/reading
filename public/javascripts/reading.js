9ecf119726c862d29763bc8b0354c6d171ba12c9
Greg Leppert <greg@formasfunction.com>
2011-08-07 14:08:19 -0400

    Wrapper script is working

--- public/javascripts/reading.js
+++ public/javascripts/reading.js
@@ -1,6 +1,10 @@
 (function($, token){
 
-var show_success = function(){
+var on_reading = (window.location.host.indexOf('reading.am') >= 0),
+    url   = on_reading ? window.location.href.split(window.location.origin)[1].substring(1) : window.location.href,
+    title = on_reading ? '' : window.document.title;
+
+var show_overlay = function(){
   var $hand = $('<span>&#9996;</span>').css({'font-size':'56px'}),
       $subtext = $('<span><br>Reading</span>'),
       $loaded_div = $('<div></div>').css({
@@ -17,17 +21,21 @@ var show_success = function(){
       }).append($hand).append($subtext);
   $('body').prepend($loaded_div);
   $loaded_div.fadeIn().delay(1000).fadeOut(400, function(){ $(this).remove(); });
-}
+};
 
 $.ajax({
   url: 'http://0.0.0.0:3000/post.json',
   dataType: 'jsonp',
-  data: {token: token, url: window.location.href, title: window.document.title},
+  data: {token: token, url: url, title: window.document.title},
   success: function(data, textStatus, jqXHR){
     if(data.meta.status == 400){
       alert('Error');
     } else {
-      show_success();
+      if(on_reading){
+        window.location = url;
+      } else {
+        show_overlay();
+      }
     }
   }
 });

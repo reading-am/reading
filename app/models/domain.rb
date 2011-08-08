b092205ca0139cccd1cda21ba445615193139d9e
Greg Leppert <greg@formasfunction.com>
2011-08-07 21:29:15 -0400

    Fixing domain pages

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,7 +1,7 @@
 class Domain < ActiveRecord::Base
-  has_many :posts
   has_many :pages
-  has_many :users, :through => :posts
+  has_many :posts, :through => :pages
+  has_many :users, :through => :pages
 
   validates_presence_of :name
 

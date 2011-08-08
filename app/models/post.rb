0d58fc09337869a76fa2a5bee2186f310f4da75d
Greg Leppert <greg@formasfunction.com>
2011-08-08 00:12:36 -0400

    Got referrer and referring posts working

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :page
   has_one     :domain, :through => :page
-  # has_many    :referring_posts
-  # has_one     :reference_post
+  has_many    :referring_posts, :class_name => 'Post',
+    :foreign_key => 'referrer_post_id'
+  belongs_to  :referrer_post, :class_name => 'Post'
 
   validates_presence_of :user, :page
 

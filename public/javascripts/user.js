a093260d6f14b31c8b58690b5efe4a113c121710
Greg Leppert <greg@formasfunction.com>
2011-08-13 18:16:09 -0400

    The following button is working

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -3,17 +3,17 @@ $(function(){
     var $this     = $(this),
         follow    = 'Follow',
         disabled  = 'disabled';
-    
+
     if($this.hasClass(disabled)) return false; //Don't take any action if it's already waiting on a call to return
 
     $this.addClass(disabled);
     $.get($this.attr('href'), function(data){
       if(data == "true"){
-        var text = $this.find('span').text() == follow ? "Un"+follow.toLowerCase() : follow,
+        var $span = $this.find('span'),
+            text = $span.text().indexOf(follow) === 0 ? "Un"+follow.toLowerCase() : follow,
             href = $this.attr('href');
-        $this
-          .attr('href', href.replace($this.text(), text.toLowerCase()))
-          .text.find('span').(text);
+        $this.attr('href', href.replace($span.text().toLowerCase(), text.toLowerCase()));
+        $span.text(text);
       }
       $this.removeClass(disabled);
     });

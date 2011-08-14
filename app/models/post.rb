006b8156acf00d22bf8d1bbca50bb4ad2fc67839
Greg Leppert <greg@formasfunction.com>
2011-08-13 20:54:58 -0400

    I think I have feeds working

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,24 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :user, :page
 
+ default_scope :order => 'posts.created_at DESC'
+
+  # Return microposts from the users being followed by the given user.
+  scope :from_users_followed_by, lambda { |user| followed_by(user) }
+
+  private
+
+  # Return an SQL condition for users followed by the given user.
+  # We include the user's own id as well.
+  def self.followed_by(user)
+    following_ids = %(SELECT followed_id FROM relationships
+                      WHERE follower_id = :user_id)
+    where("user_id IN (#{following_ids}) OR user_id = :user_id",
+          { :user_id => user })
+  end
+
+  public
+
   #TODO consider moving this to a view helper
   def wrapped_url
     "http://reading.am/p/#{Base58.encode(self.id)}/#{self.page.url}"

7efc9131681760b76b24f67c27d0ce8c82d78aa6
Greg Leppert <greg@formasfunction.com>
2011-08-20 19:11:19 -0400

    Working on Campfire hook

--- app/models/hook.rb
+++ app/models/hook.rb
@@ -1,6 +1,6 @@
 class Hook < ActiveRecord::Base
   belongs_to :user
 
-  PROVIDERS = [:hipchat, :url]
+  PROVIDERS = [:hipchat, :campfire, :url]
   validates_presence_of :provider, :token, :action
 end

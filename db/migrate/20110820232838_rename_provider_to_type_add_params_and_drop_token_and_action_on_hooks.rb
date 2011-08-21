43ff21108a9c58e27c6fe5eca34b10e1d5ccde9a
Greg Leppert <greg@formasfunction.com>
2011-08-21 01:40:52 -0400

    Working on migration

--- db/migrate/20110820232838_rename_provider_to_type_add_params_and_drop_token_and_action_on_hooks.rb
+++ db/migrate/20110820232838_rename_provider_to_type_add_params_and_drop_token_and_action_on_hooks.rb
@@ -1,11 +1,24 @@
 class RenameProviderToTypeAddParamsAndDropTokenAndActionOnHooks < ActiveRecord::Migration
   def self.up
     add_column :hooks, :params, :string
+    Hook.reset_column_information # via: http://stackoverflow.com/questions/972562/rails-wont-let-me-change-records-during-migration
+    Hook.find_all_by_provider('url').each do |hook|
+      hook.params = {:url => hook.token, :method => hook.action}.to_json
+      hook.save
+    end
+    Hook.find_all_by_provider('hipchat').each do |hook|
+      hook.params = {:token => hook.token, :room => hook.action}.to_json
+      hook.save
+    end
     rename_column :hooks, :provider, :type
+    remove_column :hooks, :token
+    remove_column :hooks, :action
   end
 
   def self.down
     remove_column :hooks, :params
     rename_column :hooks, :type, :provider
+    add_column :hooks, :token, :string
+    add_column :hooks, :action, :string
   end
 end

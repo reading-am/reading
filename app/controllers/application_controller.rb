c6b645c27229817a2e2bc47d30f9dc2c61f7f8e6
Greg Leppert <greg@formasfunction.com>
2011-09-03 21:10:08 -0400

    Setting RSS cache headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :set_time_zone
+  before_filter :set_time_zone, :set_headers
 
   helper_method :current_user, :logged_in?
 
@@ -10,6 +10,15 @@ class ApplicationController < ActionController::Base
   def set_time_zone
     min = cookies[:timezone].to_i
     Time.zone = ActiveSupport::TimeZone[-min.minutes]
+  end
+
+  def set_headers
+    if request.format == 'rss'
+      # settings borrowed from Twitter's RSS headers: https://twitter.com/statuses/user_timeline/27260086.rss
+      response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, pre-check=0, post-check=0, max-age=0"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Keep-Alive"] = "timeout=15, max=100"
+    end
   end
 
   def current_user

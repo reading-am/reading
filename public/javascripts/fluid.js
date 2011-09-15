18cfcffcd009628fdd8ec3319176891dd7c33184
Greg Leppert <greg@formasfunction.com>
2011-09-14 23:27:49 -0400

    Adding some Fluidapp files

---
+++ public/javascripts/fluid.js
@@ -0,0 +1,25 @@
+if(typeof window.fluid != 'undefined'){ (function(fluid){
+
+$(function(){ $('head').append('<link rel="stylesheet" href="stylesheets/fluid.css" type="text/css">'); });
+
+window.document.title = '✌ Reading';
+
+// fluid.dockBadge = "10";
+
+/*
+fluid.showGrowlNotification({
+    title: "title", 
+    description: "description", 
+    priority: 1, 
+    sticky: false,
+    identifier: "foo",
+    // onclick: callbackFunc,
+    // icon: imgEl // or URL string
+});
+*/
+
+})(window.fluid); }
+
+// prism
+window.platform.icon().badgeText = "33";
+window.platform.showNotification('Hey I work!', 'I work so freaking good');

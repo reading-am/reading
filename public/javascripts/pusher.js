f22aac47849e27da02e760a9d14f85dbe00eeff1
Greg Leppert <greg@formasfunction.com>
2011-09-18 16:22:42 -0400

    Working on growl notifications

--- public/javascripts/pusher.js
+++ public/javascripts/pusher.js
@@ -11,9 +11,8 @@ var pusher = new Pusher(_pusher.key);
 
 for(i = 0; i < _pusher.channels.length; i++){
   var channel = pusher.subscribe(_pusher.channels[i]);
-  channel.bind('new_post', function(data) {
-    alert(data);
-    console.log(data);
+  channel.bind('new_post', function(post) {
+    native.notify({title: post.user.display_name+' is reading', description: post.title});
   });
 }
 

9c32b5580202c85809119648b58a0e4a53c33a24
Greg Leppert <greg@formasfunction.com>
2011-10-09 18:40:45 -0400

    Fixing asset pipeline stuff

---
+++ app/assets/javascripts/app/base.js
@@ -0,0 +1,55 @@
+// write timezone info. From: http://stackoverflow.com/questions/942747/set-current-time-zone-in-rails
+if(!($.cookie('timezone'))) {
+  current_time = new Date();
+  $.cookie('timezone', current_time.getTimezoneOffset(), { path: '/', expires: 10 } );
+}
+
+window.hasfocus = true;
+$(window).focus(function(){
+  window.hasfocus = true;
+}).blur(function(){
+  window.hasfocus = false;
+});
+
+window.base58 = encdec();
+
+$(function() {
+  $("a.external").live('click', function(){
+    var $this = $(this),
+        link_host = this.href.split("/")[2],
+        document_host = document.location.href.split("/")[2],
+        base58_id = (typeof $this.data('base58_id') != 'undefined' ? $this.data('base58_id') : '');
+
+    if (link_host != document_host){
+      // old redirect method through reading url
+      // var pre = 'http://'+document_host+'/'+(base58_id ? 'p/'+base58_id+'/' : '');
+      // window.open(pre+this.href);
+
+      // new AJAX method
+      // only log it if they're logged in
+      if(current_user.logged_in()){
+        $.ajax({
+          url: '/post.json',
+          data: {
+            url: this.href,
+            referrer_id: (base58_id ? base58.decode(base58_id) : '')
+          }
+        });
+      }
+      window.open(this.href);
+      return false;
+    }
+  });
+
+  $('.bookmarklet').hover(function(){
+    var $this = $(this);
+    $this.find('span').hide();
+    $this.find('a').css('display', 'block');
+  }, function(){
+    // safari won't let you drag with this in place
+    // var $this = $(this);
+    // $this.find('span').show();
+    // $this.find('a').hide();
+  });
+
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,55 +1,12 @@
-// write timezone info. From: http://stackoverflow.com/questions/942747/set-current-time-zone-in-rails
-if(!($.cookie('timezone'))) {
-  current_time = new Date();
-  $.cookie('timezone', current_time.getTimezoneOffset(), { path: '/', expires: 10 } );
-}
-
-window.hasfocus = true;
-$(window).focus(function(){
-  window.hasfocus = true;
-}).blur(function(){
-  window.hasfocus = false;
-});
-
-window.base58 = encdec();
-
-$(function() {
-  $("a.external").live('click', function(){
-    var $this = $(this),
-        link_host = this.href.split("/")[2],
-        document_host = document.location.href.split("/")[2],
-        base58_id = (typeof $this.data('base58_id') != 'undefined' ? $this.data('base58_id') : '');
-
-    if (link_host != document_host){
-      // old redirect method through reading url
-      // var pre = 'http://'+document_host+'/'+(base58_id ? 'p/'+base58_id+'/' : '');
-      // window.open(pre+this.href);
-
-      // new AJAX method
-      // only log it if they're logged in
-      if(current_user.logged_in()){
-        $.ajax({
-          url: '/post.json',
-          data: {
-            url: this.href,
-            referrer_id: (base58_id ? base58.decode(base58_id) : '')
-          }
-        });
-      }
-      window.open(this.href);
-      return false;
-    }
-  });
-
-  $('.bookmarklet').hover(function(){
-    var $this = $(this);
-    $this.find('span').hide();
-    $this.find('a').css('display', 'block');
-  }, function(){
-    // safari won't let you drag with this in place
-    // var $this = $(this);
-    // $this.find('span').show();
-    // $this.find('a').hide();
-  });
-
-});
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree ./libs
+//= require_tree ./jquery
+//= require_tree ./app
+//= require_tree ./views

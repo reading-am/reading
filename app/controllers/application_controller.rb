45ecb781243e9cdf7c6637d0997207ed1960898b
Greg Leppert <greg@formasfunction.com>
2011-10-15 08:34:20 -0400

    Finishing up the 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -76,12 +76,12 @@ class ApplicationController < ActionController::Base
   end
 
   def not_found
-    raise  ActiveRecord::RecordNotFound.new
+    raise ActiveRecord::RecordNotFound.new
   end
 
   def show_404
     respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
+      format.html { render :file => "#{Rails.root}/public/404.html", :layout => false, :status => :not_found}
       format.xml { head :not_found }
       format.any { head :not_found }
     end

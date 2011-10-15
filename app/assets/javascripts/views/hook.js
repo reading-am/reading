856dd65c526d03634af8a50739c47f830e665e53
Greg Leppert <greg@formasfunction.com>
2011-10-15 12:03:57 -0400

    Adding facebook opengraph support

--- app/assets/javascripts/views/hook.js
+++ app/assets/javascripts/views/hook.js
@@ -2,16 +2,16 @@ $(function(){
 
   var text_field = function(name){
     return $('<div class="field">')
-              .append('<label for="hook[params]['+name.toLowerCase()+']">'+name+'</label>')
-              .append('<input name="hook[params]['+name.toLowerCase()+']">');
+              .append('<label for="hook[params]['+name.toLowerCase().replace(/ /g,'_')+']">'+name+'</label>')
+              .append('<input name="hook[params]['+name.toLowerCase().replace(/ /g,'_')+']">');
   };
   var select_field = function(name, options){
-    var $select = $('<select name="hook[params]['+name.toLowerCase()+']">');
+    var $select = $('<select name="hook[params]['+name.toLowerCase().replace(/ /g,'_')+']">');
     for(var i = 0; i < options.length; i++){
-      $select.append('<option value="'+options[i].toLowerCase()+'">'+options[i]+'</option>');
+      $select.append('<option value="'+options[i].toLowerCase().replace(/ /g,'_')+'">'+options[i]+'</option>');
     }
     return $('<div class="field">')
-              .append('<label for="hook[params]['+name.toLowerCase()+']">'+name+'</label>')
+              .append('<label for="hook[params]['+name.toLowerCase().replace(/ /g,'_')+']">'+name+'</label>')
               .append($select);
   };
 
@@ -27,6 +27,9 @@ $(function(){
         break;
       case 'campfire':
         $actions.before(text_field('Token'), text_field('Subdomain'), text_field('Room'));
+        break;
+      case 'opengraph':
+        $actions.before(text_field('Access Token'));
         break;
       case 'url':
         $actions.before(text_field('URL'), select_field('Method', ['GET', 'POST']));
--- app/models/hook.rb
+++ app/models/hook.rb
@@ -1,7 +1,7 @@
 class Hook < ActiveRecord::Base
   belongs_to :user
 
-  PROVIDERS = [:hipchat, :campfire, :url]
+  PROVIDERS = [:hipchat, :campfire, :url, :opengraph]
   validates_presence_of :provider, :params
 
   def params

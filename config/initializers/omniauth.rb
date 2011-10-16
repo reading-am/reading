9b1db0ec19bdf728983a1d0fd4d34a359797b683
Greg Leppert <greg@formasfunction.com>
2011-10-16 15:52:09 -0400

    Working on allowing multiple accounts

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'saMngnlSaaIapiznEqqw', '***REMOVED***'
-  provider :facebook, '241046329250415', '***REMOVED***', {:scope => 'email, offline_access'}
+  if Rails.env == 'production'
+    provider :facebook, '241046329250415', '***REMOVED***', {:scope => 'email, offline_access'}
+  else
+    provider :facebook, '115933145182597', '***REMOVED***', {:scope => 'email, offline_access'}
+  end
 end

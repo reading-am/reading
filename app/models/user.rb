92276933b29d400744d7c50f63203c48d266cfb5
Greg Leppert <greg@formasfunction.com>
2011-10-16 16:24:27 -0400

    Correctly check for duplicate auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,8 +26,14 @@ class User < ActiveRecord::Base
 
   def add_provider(auth_hash)
     # Check if the provider already exists, so we don't add it twice
-    unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-      Authorization.create :user => self, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+    unless Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+      Authorization.create(
+        :user       => self,
+        :provider   => auth_hash["provider"],
+        :uid        => auth_hash["uid"],
+        :token      => auth_hash["credentials"]["token"],
+        :secret     => auth_hash["credentials"]["secret"]
+      )
     end
   end
 

7003cca8abd5a24a7c04bc7a191cf032801252c8
Greg Leppert <greg@formasfunction.com>
2011-11-13 16:24:42 -0500

    Reworking config constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,14 @@
-DOMAIN = Rails.env == 'development' ? '0.0.0.0:3000' : 'reading.am'
+if Rails.env == 'production'
+  DOMAIN = 'reading.am'
+
+  FACEBOOK_KEY = '241046329250415'
+  FACEBOOK_SECRET = '***REMOVED***'
+else
+  DOMAIN = '0.0.0.0:3000'
+
+  FACEBOOK_KEY = '115933145182597'
+  FACEBOOK_SECRET = '***REMOVED***'
+end
 
 TWITTER_KEY = 'saMngnlSaaIapiznEqqw'
 TWITTER_SECRET = '***REMOVED***'

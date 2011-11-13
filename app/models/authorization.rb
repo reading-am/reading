17fe1d4cd44fdaadb8114cb5ef697ffb92662c8f
Greg Leppert <greg@formasfunction.com>
2011-11-13 09:49:14 -0500

    Adding a getter for permissions

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -10,6 +10,10 @@ class Authorization < ActiveRecord::Base
     end
   end
 
+  def permissions
+    ActiveSupport::JSON.decode(read_attribute(:permissions))
+  end
+
   def self.find_or_create(auth_hash)
     unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
       user = User.create(

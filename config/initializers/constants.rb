f2515fd9445409d0ec8994af77942ad7a09b95d9
Greg Leppert <greg@formasfunction.com>
2011-11-13 16:24:42 -0500

    Reworking config constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,14 @@
-DOMAIN = Rails.env == 'development' ? '0.0.0.0:3000' : 'reading.am'
+if Rails.env == 'production'
+  DOMAIN = 'reading.am'
+
+  FACEBOOK_KEY = '241046329250415'
+  FACEBOOK_SECRET = 'dedc9c174b5f712fe29bde26df8f8cdf'
+else
+  DOMAIN = '0.0.0.0:3000'
+
+  FACEBOOK_KEY = '115933145182597'
+  FACEBOOK_SECRET = 'da0fac5ca6afbe272f51e297d3380dce'
+end
 
 TWITTER_KEY = 'saMngnlSaaIapiznEqqw'
 TWITTER_SECRET = '17tgkJvgwwdKplVqSfCHKTwsOjbaMuCW4Rxe3rrDkA'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, TWITTER_KEY, TWITTER_SECRET
-  if Rails.env == 'production'
-    provider :facebook, '241046329250415', 'dedc9c174b5f712fe29bde26df8f8cdf', {:scope => 'email, offline_access'}
-  else
-    provider :facebook, '115933145182597', 'da0fac5ca6afbe272f51e297d3380dce', {:scope => 'email, offline_access'}
-  end
+  provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, {:scope => 'email, offline_access'}
 end

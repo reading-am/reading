eaf0330e6a7e6a6556c0f146ed6b889432b022f2
Greg Leppert <greg@formasfunction.com>
2011-11-24 11:33:17 -0500

    Working on hook constructor

--- app/assets/javascripts/views/hook.js
+++ app/assets/javascripts/views/hook.js
@@ -1,51 +1,76 @@
+var hook_properties = {
+  "actions":[
+    {"name":"any", "text":"read, \"yep\" or \"nope\""},
+    {"name":"read", "text":"read a page"},
+    {"name":"yep",  "text":"say \"yep\""},
+    {"name":"nope", "text":"say \"nope\""}
+  ],
+  "providers":[
+    {"name":"Campfire", "params":[
+      {"name":"room"},
+      {"name":"token"}
+    ]},
+    {"name":"email", "params":[
+      {"name":"address"}
+    ]},
+    {"name":"Facebook", "params":[
+      {"name":"account"}
+    ]},
+    {"name":"HipChat", "params":[
+      {"name":"token"},
+      {"name":"room"}
+    ]},
+    {"name":"Twitter", "params":[
+      {"name":"account"}
+    ]},
+    {"name":"URL", "params":[
+      {"name":"address"},
+      {
+        "name":"method",
+        "options":["POST","GET"]
+      }
+    ]}
+  ]
+};
+
+var select_field = function(param, scope){
+  var name = param.name.toLowerCase().replace(/ /g,'_');
+  if(scope) name = scope+'['+name+']';
+  var $select = $('<select name="'+name+'">');
+  for(var i = 0; i < param.options.length; i++){
+    var op = param.options[i];
+    $select.append($('<option>').val(op.name.toLowerCase()).text(op.text ? op.text : op.name));
+  }
+  return $select;
+},
+text_field = function(param, scope){
+  var name = param.name.toLowerCase().replace(/ /g,'_');
+  if(scope) name = scope+'['+name+']';
+  return $('<span class="field">')
+            .append(' <label for="'+name+'">'+param.name+'</label> ')
+            .append(' <input type="text" name="'+name+'" placeholder="'+param.name+'"> ');
+},
+build_provider_params = function(params){
+  var $prov = $('<span>').attr('id','provider_params');
+  for(var i = 0; i < params.length; i++){
+    if(i === 1) $prov.append(' with ');
+    $prov.append(text_field(params[i], 'hook[params]'));
+  }
+  return $prov;
+};
+
 $(function(){
 
-  var text_field = function(name){
-    return $('<div class="field">')
-              .append('<label for="hook[params]['+name.toLowerCase().replace(/ /g,'_')+']">'+name+'</label>')
-              .append('<input name="hook[params]['+name.toLowerCase().replace(/ /g,'_')+']">');
-  },
-  select_field = function(name, options){
-    var $select = $('<select name="hook[params]['+name.toLowerCase().replace(/ /g,'_')+']">');
-    for(var i = 0; i < options.length; i++){
-      $select.append('<option value="'+options[i].toLowerCase().replace(/ /g,'_')+'">'+options[i]+'</option>');
-    }
-    return $('<div class="field">')
-              .append('<label for="hook[params]['+name.toLowerCase().replace(/ /g,'_')+']">'+name+'</label>')
-              .append($select);
-  };
+  $('#constructor .wrapper')
+    .append('When I ')
+    .append(select_field({"name":"action", "options":hook_properties.actions}, 'hook'))
+    .append(' please post to ')
+    .append(select_field({"name":"provider", "options":hook_properties.providers}, 'hook'))
+    .append(build_provider_params(hook_properties.providers[0].params));
 
-  $("#hook_provider").change(function(){
-    var $this = $(this),
-        $actions = $('.actions');
-    $('.field:not(:first)').remove();
-    $('.footnote').data('url', '/footnotes/'+$this.val());
-    switch($this.val()){
-      case 'hipchat':
-        $actions.before(text_field('Token'), text_field('Room'));
-        break;
-      case 'campfire':
-        $actions.before(text_field('Token'), text_field('Subdomain'), text_field('Room'));
-        break;
-      case 'opengraph':
-        if(has_facebook){
-          $actions.before(
-            $('<div class="field">')
-                .append('<label for="hook[params][when]">Press Create!</label>')
-                .append('<input name="hook[params][when]" val="on_post" type="hidden">')
-          );
-        } else {
-          $actions.before('<div class="field"><a href="/auth/facebook">First, click here to connect your Facebook account</a></div>');
-        }
-        break;
-      case 'url':
-        $actions.before(text_field('URL'), select_field('Method', ['GET', 'POST']));
-        break;
-    }
-  });
-
-  $('#action_select').change(function(){
-    $('#hook_action').val($(this).val());
+  var $provider = $("#constructor select[name='hook[provider]']").change(function(){
+    $('.footnote').data('url', '/footnotes/'+$provider.val());
+    $('#provider_params').replaceWith(build_provider_params(hook_properties.providers[$provider.prop('selectedIndex')].params));
   });
 
 });

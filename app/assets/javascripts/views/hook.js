e68b2e9c2b25e593bc71caf0241202bfdd70b206
Greg Leppert <greg@formasfunction.com>
2011-11-24 15:09:33 -0500

    Working on hooks constructor

--- app/assets/javascripts/views/hook.js
+++ app/assets/javascripts/views/hook.js
@@ -7,24 +7,24 @@ var hook_properties = {
   ],
   "providers":[
     {"name":"Campfire", "params":[
-      {"name":"room"},
-      {"name":"token"}
+      {"name":"room", "placeholder": "My Room Name"},
+      {"name":"token", "placeholder":"12345abcdefg67890abcdefg12345abcdefg6789"}
     ]},
     {"name":"email", "params":[
-      {"name":"address"}
+      {"name":"address", "placeholder":"me@example.com"}
     ]},
     {"name":"Facebook", "params":[
       {"name":"account"}
     ]},
     {"name":"HipChat", "params":[
-      {"name":"token"},
-      {"name":"room"}
+      {"name":"room", "placeholder":"My Room Name"},
+      {"name":"token", "placeholder":"12345abcdefg67890abcdefg12345a"}
     ]},
     {"name":"Twitter", "params":[
       {"name":"account"}
     ]},
     {"name":"URL", "params":[
-      {"name":"address"},
+      {"name":"address", "placeholder":"http://example.com"},
       {
         "name":"method",
         "options":["POST","GET"]
@@ -38,7 +38,7 @@ var select_field = function(param, scope){
   if(scope) name = scope+'['+name+']';
   var $select = $('<select name="'+name+'">');
   for(var i = 0; i < param.options.length; i++){
-    var op = param.options[i];
+    var op = (typeof param.options[i] == 'string' ? {"name":param.options[i]} : param.options[i]);
     $select.append($('<option>').val(op.name.toLowerCase()).text(op.text ? op.text : op.name));
   }
   return $select;
@@ -48,13 +48,14 @@ text_field = function(param, scope){
   if(scope) name = scope+'['+name+']';
   return $('<span class="field">')
             .append(' <label for="'+name+'">'+param.name+'</label> ')
-            .append(' <input type="text" name="'+name+'" placeholder="'+param.name+'"> ');
+            .append(' <input type="text" name="'+name+'" placeholder="'+(param.placeholder ? param.placeholder : param.name)+'"> ');
 },
 build_provider_params = function(params){
   var $prov = $('<span>').attr('id','provider_params');
   for(var i = 0; i < params.length; i++){
-    if(i === 1) $prov.append(' with ');
-    $prov.append(text_field(params[i], 'hook[params]'));
+    if(i === 1) $prov.append(' using ');
+    var func = (params[i].options ? select_field : text_field);
+    $prov.append(func(params[i], 'hook[params]'));
   }
   return $prov;
 };

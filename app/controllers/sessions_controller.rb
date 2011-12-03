99f4ab6c8f27a4f4a503f8bafd74c02aa659a431
Greg Leppert <greg@formasfunction.com>
2011-12-03 17:25:36 -0500

    Adding add remove auth permissions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,14 @@ class SessionsController < ApplicationController
         notice = "You can now login using this #{auth_hash["provider"].capitalize} account"
       rescue Exception => e
         notice = e.message
+        # TODO - clean this up. This is to migrate over twitter users who had the previous read-only permissions
+        if auth_hash["provider"] == 'twitter'
+          auth = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+          if !auth.can :write
+            auth.add_perm :write
+            auth.save
+          end
+        end
       end
 
       if path = cookies[:session_create_redirect]

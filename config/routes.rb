ccadfb51880bb63f98ec7a928356486fe408b364
Greg Leppert <greg@formasfunction.com>
2011-12-10 14:12:40 -0500

    Adding pagination

--- config/routes.rb
+++ config/routes.rb
@@ -35,8 +35,8 @@ Reading::Application.routes.draw do
   end
   # match '/:username' => 'users#show'
   # These routes should be cleaned up
-  match '/:username(/posts)'  => 'users#show', :defaults => { :type => 'posts' }
-  match '/:username/list'     => 'users#show', :defaults => { :type => 'list' }
+  match '/:username(/posts)(/page/:page)'  => 'users#show', :defaults => { :type => 'posts', :page => 1 }
+  match '/:username/list(/page/:page)'     => 'users#show', :defaults => { :type => 'list', :page => 1 }
   match '/:username/info'     => 'users#edit'
   match '/:username/hooks'    => 'users#hooks'
   match '/:username/export'   => 'users#export'

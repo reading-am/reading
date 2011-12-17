bac431417ee89a365efb3c88c7068c409c691ed2
Greg Leppert <greg@formasfunction.com>
2011-12-17 14:51:50 -0500

    Adding mail flag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111216080658) do
+ActiveRecord::Schema.define(:version => 20111217194935) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(:version => 20111216080658) do
     t.string   "image"
     t.string   "phone"
     t.string   "urls"
+    t.boolean  "wants_mail",  :default => true
   end
 
 end

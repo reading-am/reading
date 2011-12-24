85ce92c23f87de1913e9b5f72e6daa7e7728016d
Greg Leppert <greg@formasfunction.com>
2011-12-23 21:02:30 -0500

    Spinning out loader css

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     if Rails.env == 'production'
       "javascript:reading={};(function(){reading.token='#{user.token}';var otherlib=(typeof jQuery=='undefined'&&typeof $=='function');function getScript(url,success){var script=document.createElement('script');script.src=url;var head=document.getElementsByTagName('head')[0],done=false;script.onload=script.onreadystatechange=function(){if(!done&&(!this.readyState||this.readyState=='loaded'||this.readyState=='complete')){done=true;success();script.onload=script.onreadystatechange=null;head.removeChild(script);}};head.appendChild(script);}getScript('http://code.jquery.com/jquery-latest.min.js',function(){if(typeof jQuery=='undefined'){alert('There was an error loading Reading');}else{jQuery.getScript('http://#{DOMAIN}/javascripts/reading.js');}});})();"
     else
-      "javascript:var%20reading={token:'#{user.token}'},head=document.getElementsByTagName('head')[0],script=document.createElement('script');script.src='http://#{DOMAIN}/assets/loader.js';head.appendChild(script);"
+      "javascript:(function(){window.reading={token:'#{user.token}'};var head=document.getElementsByTagName('head')[0],script=document.createElement('script');script.src='http://#{DOMAIN}/assets/loader.js';head.appendChild(script);})()"
     end
   end
 end

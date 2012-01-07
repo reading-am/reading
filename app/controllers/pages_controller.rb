f06052bdd3639df622c283c5c650dee0974bc8f9
Greg Leppert <greg@formasfunction.com>
2012-01-07 16:49:32 -0500

    Adding a page redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,13 @@ class PagesController < ApplicationController
 
   def show
     @page = Page.find(params[:id])
-
-    respond_to do |format|
-      format.html  # show.html.erb
-      format.json  { render :json => @page }
+    if @page.domain.name != params[:domain_id]
+      redirect_to "/domains/#{@page.domain.name}/pages/#{@page.id}"
+    else
+      respond_to do |format|
+        format.html  # show.html.erb
+        format.json  { render :json => @page }
+      end
     end
   end
 

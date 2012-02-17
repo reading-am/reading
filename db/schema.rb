902daed23d4c12493ed375ed839eb3e12d8dffcc
Greg Leppert <greg@formasfunction.com>
2012-02-17 09:18:11 -0500

    Add readability support

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120208155633) do
+ActiveRecord::Schema.define(:version => 20120217133743) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20120208155633) do
     t.integer  "domain_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "readability"
   end
 
   create_table "posts", :force => true do |t|

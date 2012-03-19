4f3ad58fd3482850eb75047dddb7bd31eae512cd
Greg Leppert <greg@formasfunction.com>
2012-03-19 01:20:46 -0400

    Adding hirefire

---
+++ config/initializers/hirefire.rb
@@ -0,0 +1,9 @@
+HireFire.configure do |config|
+  config.max_workers = 10
+  config.job_worker_ratio = [
+    { :when => lambda {|jobs| jobs < 15 }, :workers => 1 },
+    { :when => lambda {|jobs| jobs < 35 }, :workers => 2 },
+    { :when => lambda {|jobs| jobs < 60 }, :workers => 3 },
+    { :when => lambda {|jobs| jobs < 80 }, :workers => 4 }
+  ]
+end

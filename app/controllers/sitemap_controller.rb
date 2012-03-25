36166427820b23ac4084b2e95fb1da214c3e2ffb
Greg Leppert <greg@formasfunction.com>
2012-03-24 21:42:43 -0400

    Serve all sitemaps as redirects

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,7 @@
 # per: http://www.billrowell.com/2012/02/01/create-an-xml-sitemap-on-heroku-via-amazon-s3/
 class SitemapController < ApplicationController
-   def index
-      redirect_to "http://reading-#{Rails.env}.s3.amazonaws.com/sitemaps/sitemap_index.xml.gz"
-   end
+  def index
+    params[:partial] = '_index' if params[:partial].nil?
+    redirect_to "http://reading-#{Rails.env}.s3.amazonaws.com/sitemaps/sitemap#{params[:partial]}.xml.gz"
+  end
 end

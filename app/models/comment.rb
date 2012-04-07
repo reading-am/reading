324e87035204bdead591b766a7b6827eea8f7749
Greg Leppert <greg@formasfunction.com>
2012-04-07 14:21:03 -0400

    Adding comment formatting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   include Twitter::Extractor
+  include Twitter::Autolink
 
   belongs_to :user
   belongs_to :post
@@ -12,11 +13,20 @@ class Comment < ActiveRecord::Base
     extract_mentioned_screen_names body
   end
 
-  def tags
+  def hashtags
     extract_hashtags body
   end
 
   def urls
     extract_urls body
+  end
+
+  def body_html
+    auto_link(html_escape(body), {
+      :url_class => '',
+      :username_class => 'user',
+      :username_url_base => "//#{DOMAIN}/",
+      :hashtag_url_base => "//#{DOMAIN}/search?q="
+    }).html_safe
   end
 end

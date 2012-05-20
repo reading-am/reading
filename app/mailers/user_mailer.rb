9245ca2096d688110320167acb9711653aff9cf8
Greg Leppert <greg@formasfunction.com>
2012-05-19 20:25:02 -0400

    Fixing digest utf8 characters

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -24,7 +24,7 @@ class UserMailer < ActionMailer::Base
   def digest(user)
     @user   = user
     @posts  = @user.unread_since(@user.mail_digest.days.ago).limit(30)
-    #@posts  = @user.unread_since(15.days.ago).limit(10) # for testing
+    #@posts  = @user.unread_since(9999.days.ago).limit(100) # for testing
     mail(
       :to       => @user.email,
       :subject  => "Your Reading Digest - #{Time.now.strftime("%b %d, %Y")}"

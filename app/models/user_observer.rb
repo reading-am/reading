f1b5458060739a3e0a133c66dedbe3aa5a3c1aa7
Greg Leppert <greg@formasfunction.com>
2012-07-02 21:32:18 -0400

    Working on auto following

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,9 +1,18 @@
 class UserObserver < ActiveRecord::Observer
 
   def before_save(user)
+    # when it's a new user and they have a username
     if Rails.env == 'production' && !user.username.blank? && user.username_was.blank?
+
+      # Tweet to ReadingArrivals
       tweet = "Everyone welcome #{user.username}! http://#{DOMAIN}/#{user.username}"
       Twitter::Client.new(:oauth_token => "587119018-Xr0zC5OEqtmIV9MlmUozwvHNZZDZbQ7CwiY2fOQs", :oauth_token_secret => "Kqm1Mzn3HF8HafhLRIlMOJUiPmssD7gGeaVk1SvAmJ4").delay.update tweet rescue nil
+
+      # Auto-follow everyone from their social network
+      user.authorizations.first.following.each do |u|
+        user.follow!(u)
+      end
+
     end
   end
 

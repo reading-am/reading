db1b9619af1e63e6839615c9497b4d36acf47a11
Greg Leppert <greg@formasfunction.com>
2012-08-01 18:42:52 -0400

    Adding provider styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,6 +33,8 @@ module ApplicationHelper
         output = content_tag :span, 'I', :class => 'icon'
       when 'readability'
         output = content_tag :span, '♣', :class => 'icon'
+      when 'kippt'
+        output = content_tag :span, 'k', :class => 'icon'
       else
         output = content_tag :span
       end

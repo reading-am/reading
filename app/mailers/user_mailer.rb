9b78928d343d39a83d5b0240490314c76a3f8ce8
Greg Leppert <greg@formasfunction.com>
2012-08-06 00:27:24 -0400

    Merging automatically-give-comments-access-to-mentioned-users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -31,6 +31,17 @@ class UserMailer < ActionMailer::Base
     )
   end
 
+  def comments_welcome(user, comment)
+    @user = user
+    @comment = comment
+
+    mail(
+      :to       => @user.email,
+      :from     => "Greg & Max <greg-and-max@reading.am>",
+      :subject  => "Help Us Test Comments!"
+    )
+  end
+
   def welcome(user)
     @user = user
     mail(

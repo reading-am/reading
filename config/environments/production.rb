d4bab067ff6e1fcd5a8f049baafd73b47962caef
Greg Leppert <greg@formasfunction.com>
2012-08-11 09:41:58 -0400

    Trying to get production working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,11 @@ Reading::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
+  # requirejs namespacing needs it to be false
   config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -43,7 +44,7 @@ Reading::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['*.js*', '*.css*']
+  config.assets.precompile += ['*.css*']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

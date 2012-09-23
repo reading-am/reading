a50fc8110126156e96abce98cd49fd0ff6fbc5f5
Greg Leppert <greg@formasfunction.com>
2012-09-23 18:08:18 -0400

    Merging 292-enable-posting-via-email closes #292

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -135,6 +135,7 @@ class UsersController < ApplicationController
   def extras
     redirect_to "/" and return if !logged_in?
     @user = current_user
+    @post_email = MailPipe::encode_mail_recipient('post', current_user, current_user)
   end
 
   def find_people

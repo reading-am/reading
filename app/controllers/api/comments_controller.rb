d61009479c3012534de0d603e1109ae4038b469c
Greg Leppert <greg@formasfunction.com>
2012-09-30 22:20:32 -0400

    Move comment hooks and email to an observer

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -62,25 +62,6 @@ class Api::CommentsController < Api::APIController
 
     respond_to do |format|
       if @comment.save
-        # We treat Pusher just like any other hook except that we don't store it
-        # with the user so we go ahead and construct one here
-        event = :comment
-        Hook.new({:provider => 'pusher', :events => [:new,:yep,:nope]}).run(@comment, event)
-        @comment.user.hooks.each do |hook| hook.run(@comment, event) end
-
-        User.mentioned_in(@comment).where('id != ?', @comment.user.id).each do |user|
-          if !user.access? :comments
-            user.access << :comments
-            user.save
-            UserMailer.delay.comments_welcome(user, @comment) if !user.email.blank?
-          end
-
-          if !user.email.blank? and user.email_when_mentioned
-            @comment.is_a_show ? UserMailer.delay.shown_a_page(@comment, user)
-                               : UserMailer.delay.mentioned(@comment, user)
-          end
-        end
-
         format.json { render_json({:comment => @comment.simple_obj}, :created) }
       else
         # TODO clean up this auth hack. Ugh.
---
+++ app/models/comment_observer.rb
@@ -0,0 +1,28 @@
+class CommentObserver < ActiveRecord::Observer
+
+  def after_save(comment)
+    # We treat Pusher just like any other hook except that we don't store it
+    # with the user so we go ahead and construct one here
+    event = :comment
+    Hook.new({:provider => 'pusher', :events => [:new,:yep,:nope,:comment]}).run(comment, event)
+    comment.user.hooks.each do |hook| hook.run(comment, event) end
+
+    # These are has_many relationships
+    Pusher["pages.#{comment.page_id}.comments"].trigger('create', comment.simple_obj)
+
+    # Send mention emails
+    User.mentioned_in(comment).where('id != ?', comment.user_id).each do |user|
+      if !user.access? :comments
+        user.access << :comments
+        user.save
+        UserMailer.delay.comments_welcome(user, comment) if !user.email.blank?
+      end
+
+      if !user.email.blank? and user.email_when_mentioned
+        comment.is_a_show ? UserMailer.delay.shown_a_page(comment, user)
+                           : UserMailer.delay.mentioned(comment, user)
+      end
+    end
+  end
+
+end

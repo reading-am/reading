ee1ca1b8de5b41db0acc3c8be00a918d50332487
Greg Leppert <greg@formasfunction.com>
2012-11-03 10:28:40 -0400

    Merging give-girl_friday-a-try

--- lib/modules/broadcaster.rb
+++ lib/modules/broadcaster.rb
@@ -1,11 +1,8 @@
 module Broadcaster
 
   def self.signal action, obj
-    # If we have an eventmachine loop, use async triggering
-    method = (defined?(EventMachine) && EventMachine.reactor_running?) ? 'trigger_async' : 'trigger'
-
     simple_obj = obj.simple_obj
-    obj.channels.each do |channel| Pusher[channel].send(method, action, simple_obj) end
+    obj.channels.each do |channel| PUSHER_QUEUE << {:channel => channel, :action => action, :msg => simple_obj} end
   end
 
 end

b8084a5fd92cf1f15d7027eb0db7efd7f59f118a
Greg Leppert <greg@formasfunction.com>
2012-11-05 02:09:01 -0500

    Compensate for the lack of a PUSHER_QUEUE in the console

--- lib/modules/broadcaster.rb
+++ lib/modules/broadcaster.rb
@@ -1,8 +1,17 @@
 module Broadcaster
 
   def self.signal action, obj
+    async = defined? PUSHER_QUEUE # this won't exist in the console
     simple_obj = obj.simple_obj
-    obj.channels.each do |channel| PUSHER_QUEUE << {:channel => channel, :action => action, :msg => simple_obj} end
+
+    obj.channels.each do |channel|
+      msg = {:channel => channel, :action => action, :msg => simple_obj}
+      if async
+        PUSHER_QUEUE << msg
+      else
+        Pusher[msg[:channel]].trigger(msg[:action], msg[:msg])
+      end
+    end
   end
 
 end

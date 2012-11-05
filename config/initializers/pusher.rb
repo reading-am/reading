f1ebc7de758e2004b28e4510ef1aa57f13a91dfc
Greg Leppert <greg@formasfunction.com>
2012-11-05 02:39:48 -0500

    Temporarily switch back to Pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,12 +1,12 @@
 case Rails.env
-when 'production'
-  Pusher.host   = 'ws.reading.am'
+#when 'production'
+  #Pusher.host   = 'ws.reading.am'
 when 'development'
   Pusher.host   = 'localhost'
 end
 
 case Rails.env
-when 'production', 'development'
+when 'development'#, 'production'
   Pusher.port   = 4567
   Pusher.key    = ENV['READING_SLANGER_KEY']
   Pusher.secret = ENV['READING_SLANGER_SECRET']

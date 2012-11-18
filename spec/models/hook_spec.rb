e403af17df93f0d71a25f4bb9407f5daa9004ae6
Greg Leppert <greg@formasfunction.com>
2012-11-18 18:48:14 -0500

    Delete tweet after testing

--- spec/models/hook_spec.rb
+++ spec/models/hook_spec.rb
@@ -4,13 +4,17 @@ describe Hook do
   fixtures :hooks, :posts, :pages, :domains, :authorizations
   context "when run" do
     it "posts to Twitter" do
+      # setup
       hook = hooks(:twitter)
       hook.authorization = authorizations(:twitter)
       post = posts(:one)
       post.page = pages(:daringfireball)
       post.page.domain = domains(:daringfireball)
-
-      hook.run(post, :new).should be_an_instance_of Twitter::Tweet
+      # test
+      response = hook.run(post, :new)
+      response.should be_an_instance_of Twitter::Tweet
+      #cleanup
+      hook.authorization.api.status_destroy(response.id)
     end
   end
 end

84c5f5d063fd3dfda9809151e56a46339966a8d2
Greg Leppert <greg@formasfunction.com>
2012-11-22 10:46:02 -0500

    Change s3 config to a constant and rename init so that it loads before any others

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,9 +57,9 @@ module ApplicationHelper
       when 'chrome'
         link = 'https://chrome.google.com/webstore/detail/npjdbbeldblbjenemjdeplmlaieifjhk'
       when 'safari'
-        link = 'http://reading-production.s3.amazonaws.com/extensions/reading.safariextz'
+        link = "#{S3_BUCKET}/extensions/reading.safariextz"
       when 'firefox'
-        link = 'http://reading-production.s3.amazonaws.com/extensions/reading.xpi'
+        link = "#{S3_BUCKET}/extensions/reading.xpi"
     end
     link_to text, link, :class => "btn #{browser}-install"
   end

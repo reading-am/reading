28b1e368f7fee10f399131cc4e64ad0a92fd012a
Steve Kaliski <sjkaliski@gmail.com>
2013-01-22 15:14:09 -0500

    properly migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206195529) do
+ActiveRecord::Schema.define(:version => 20130122200825) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"

54960a8a3a9e1284ba19432a11a0534c260ab20d
Greg Leppert <greg@formasfunction.com>
2013-02-09 14:43:23 -0500

    Adding tests for keywords and type

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -21,6 +21,21 @@ describe Page do
       page.title_tag.should eq("Slint - \"Nosferatu Man\" - YouTube")
     end
 
+    it "accurately parses the media type when present" do
+      page = pages(:youtube_short)
+      page.type.should eq("video")
+    end
+
+    it "accurately parses the keywords into an array when present" do
+      page = pages(:youtube_short)
+      page.keywords.should eq([
+        'slint','louisville','indie',
+        'punk','rock','hardcore',
+        'kentucky','reunion','pajo',
+        'walford','nosferatu'
+      ])
+    end
+
     it "accurately parses the meta tags when present" do
       page = pages(:youtube_short)
       page.meta_tags.should eq({

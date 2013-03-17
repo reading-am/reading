011c02c043ba7f0ec83471501e7f486513650ff3
Greg Leppert <greg@formasfunction.com>
2013-03-17 14:01:43 -0400

    Add url cleanup migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130317032339) do
+ActiveRecord::Schema.define(:version => 20130317161100) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"

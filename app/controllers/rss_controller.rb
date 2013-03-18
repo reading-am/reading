924d93cca97e36fd43e108f2911f9971626f336f
Greg Leppert <greg@formasfunction.com>
2013-03-17 21:16:51 -0400

    RSS feed hacked in, working for www.nytimes.com

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -4,8 +4,26 @@ class RssController < ApplicationController
   # GET /domains/1.xml
   def show
     @page = Page.find_by_url("#{params[:url]}/")
-    debugger
-    @posts = @page.posts.order("created_at DESC").paginate(:page => params[:page])
+
+    @posts = []
+    user = User.find(1)
+    i = 1
+
+    @page.remote_rss.search("item").each do |item|
+      post = Post.new(
+        :user => user,
+        :page => Page.new(
+          :url => item.search("link").first.content,
+          :title => item.search("title").first.content
+        ),
+        :created_at => Time.now,
+        :updated_at => Time.now
+      )
+      post.id = i
+      post.page.id = i
+      i += 1
+      @posts << post
+    end
 
     respond_to do |format|
       format.html { render 'posts/index' }

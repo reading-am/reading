7ddab69a251e8f92b273656fc53abce39ffa4a74
Greg Leppert <greg@formasfunction.com>
2013-03-29 09:56:38 -0400

    Move javascripts/backbone to javascripts/app

--- config/initializers/stache.rb
+++ config/initializers/stache.rb
@@ -1,4 +1,4 @@
 Stache.configure do |config|
-  config.template_base_path = File.join(Rails.root, "app", "assets", "javascripts", "backbone", "templates")
+  config.template_base_path = File.join(Rails.root, "app", "assets", "javascripts", "app", "templates")
   config.use :mustache
 end

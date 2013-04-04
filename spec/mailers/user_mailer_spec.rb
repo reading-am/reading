8a110d163d25e230141e3cbc860962c921c00626
Greg Leppert <greg@formasfunction.com>
2013-04-04 15:28:13 -0400

    Add mailer sniff tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,6 +1,45 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  fixtures :users, :comments, :pages
 
+  it "should deliver new follower mail" do
+    UserMailer.new_follower(users(:greg), users(:howard)).deliver
+  end
+
+  it "should deliver mentioned mail" do
+    comment = comments(:single_mention)
+    comment.user = users(:howard)
+    comment.page = pages(:daringfireball)
+
+    UserMailer.mentioned(comment, users(:greg)).deliver
+  end
+
+  it "should deliver shown a page mail" do
+    comment = comments(:single_show)
+    comment.user = users(:howard)
+    comment.page = pages(:daringfireball)
+
+    UserMailer.shown_a_page(comment, users(:greg)).deliver
+  end
+
+  it "should deliver comments welcome mail" do
+    comment = comments(:single_show)
+    comment.user = users(:howard)
+
+    UserMailer.comments_welcome(users(:greg), comment).deliver
+  end
+
+  it "should deliver welcome mail" do
+    UserMailer.welcome(users(:greg)).deliver
+  end
+
+  it "should deliver destroyed mail" do
+    UserMailer.destroyed(users(:greg)).deliver
+  end
+
+  it "should deliver digest mail" do
+    UserMailer.digest(users(:greg)).deliver
+  end
+
+end

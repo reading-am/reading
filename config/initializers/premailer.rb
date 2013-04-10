3473bdcacac317991c2c216deb52c09bd946fdf2
Greg Leppert <greg@formasfunction.com>
2013-04-10 15:46:22 -0400

    Turn off premailer verbose for testing

--- config/initializers/premailer.rb
+++ config/initializers/premailer.rb
@@ -1,5 +1,5 @@
 Premailer::Rails.config.merge!(
   :generate_text_part => false,
   :warn_level => Premailer::Warnings::RISKY,
-  :verbose => Rails.env != "production"
+  :verbose => Rails.env == "development"
 )

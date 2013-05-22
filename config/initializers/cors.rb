c9e23685d0b205a202c74168914a8eeca979152f
Greg Leppert <greg@formasfunction.com>
2013-05-21 22:23:50 -0400

    Spin Rack::Cors out into an initializer and insert it at the top of the middleware stack

---
+++ config/initializers/cors.rb
@@ -0,0 +1,18 @@
+# NOTE - this needs to be inserted at the top of the Rack middleware stack,
+# before Rack::Cache or else it fails to insert the headers. Gleaned from:
+# https://github.com/diaspora/diaspora/pull/2990
+
+Rails.application.config.middleware.insert 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource %r{api/.*},
+      :headers => :any,
+      :methods => [:get, :post, :put, :delete]
+    resource '/pusher/auth',
+      :headers => :any,
+      :methods => [:get, :post]
+    resource %r{assets/.*},
+      :headers => :any,
+      :methods => :get
+  end
+end

a4c99495ce3cb122b0d2ea6adb2baa41a7848a47
Greg Leppert <greg@formasfunction.com>
2013-06-26 13:17:02 -0400

    Update delayed_job to add queues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609172058) do
+ActiveRecord::Schema.define(:version => 20130626171533) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(:version => 20130609172058) do
     t.string   "locked_by"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "queue"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"

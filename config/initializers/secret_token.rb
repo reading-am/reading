0c0634079fdb92c26f6341e385a14711bf1df490
Greg Leppert <greg@formasfunction.com>
2013-06-27 12:12:13 -0400

    Rename secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Reading::Application.config.secret_token = '3a0fa968d1cc7548fa923141393e9d6d6ed8fe0fc8caff5642ebe68ebfaf8f204b67eaf795803f4bf4d22e25e12ff4ce1bf17a6e54289e575692ffddac726e01'
+Reading::Application.config.secret_key_base = '3a0fa968d1cc7548fa923141393e9d6d6ed8fe0fc8caff5642ebe68ebfaf8f204b67eaf795803f4bf4d22e25e12ff4ce1bf17a6e54289e575692ffddac726e01'

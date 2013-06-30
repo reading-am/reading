81617cbbb0215891dbdc818407778a814faabf98
Greg Leppert <greg@formasfunction.com>
2013-06-30 12:52:12 -0400

    Switch to Heroku's recommended unicorn setup

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,37 +1,25 @@
 # config/unicorn.rb
-# from: https://gist.github.com/1401792
-
-# See comment by @paulelliott
 worker_processes 3
-timeout 30
+timeout 15
 preload_app true
 
 before_fork do |server, worker|
-  # Replace with MongoDB or whatever
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info('Disconnected from ActiveRecord')
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
 
-  # If you are using Redis but not Resque, change this
-  if defined?(Resque)
-    Resque.redis.quit
-    Rails.logger.info('Disconnected from Redis')
-  end
-end
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end 
 
 after_fork do |server, worker|
-  # Replace with MongoDB or whatever
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-    Rails.logger.info('Connected to ActiveRecord')
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  # If you are using Redis but not Resque, change this
-  if defined?(Resque)
-    Resque.redis = ENV['REDIS_URI']
-    Rails.logger.info('Connected to Redis')
-  end
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 
   # Config girl_friday queues
   # needs to be here when used with unicorn and preload_app true

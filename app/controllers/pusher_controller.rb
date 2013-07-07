eb9e01e448a15caa8e73b1c6e28963f478166aa6
Greg Leppert <greg@formasfunction.com>
2013-07-07 16:16:43 -0400

    Upgrade pusher js lib and fix auth endpoint

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -5,28 +5,27 @@ class PusherController < Api::APIController
   def auth
     @user = params[:token] ? User.find_by_token(params[:token]) : current_user
     if !@user.blank?
-      response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {
-        :user_id => @user.id, # => required
-        #:user_info => { # => optional - for example
-          #:name => @user.name
-        #}
-      })
-      render :json => response, :callback => params[:callback]
+      response = Pusher[params[:channel_name]].authenticate(params[:socket_id], {user_id: @user.id})
+      render json: response, callback: params[:callback]
     else
-      render :text => "Forbidden", :status => '403'
+      render text: 'Forbidden', status: 403
     end
   end
   add_transaction_tracer :auth
 
   def existence
-    event = ActiveSupport::JSON.decode(request.raw_post)['events'].first
-    if (event['channel'] =~ /^users\.[0-9]*\.feed$/) == 0 \
-      and ['channel_occupied','channel_vacated'].include? event['name']
-      User.where(:id => event['channel'].split('.')[1])
-          .update_all(:feed_present => (event['name'] == 'channel_occupied'))
-    end
-    respond_to do |format|
-      format.html {head :ok}
+    webhook = Pusher.webhook(request)
+    if webhook.valid?
+      webhook.events.each do |event|
+        if (event['channel'] =~ /^users\.[0-9]*\.feed$/) == 0 \
+          and ['channel_occupied','channel_vacated'].include? event['name']
+          User.where(:id => event['channel'].split('.')[1])
+              .update_all(:feed_present => (event['name'] == 'channel_occupied'))
+        end
+      end
+      render text: 'ok'
+    else
+      render text: 'invalid', status: 401
     end
   end
   add_transaction_tracer :existence

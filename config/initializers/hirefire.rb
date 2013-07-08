5404dde612cb1dc2daa7e6ba2e060d8371b9dc45
Greg Leppert <greg@formasfunction.com>
2013-07-08 01:16:52 -0400

    Switch to the new HireFire

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,9 +1,5 @@
-# HireFire.configure do |config|
-#   config.max_workers = 10
-#   config.job_worker_ratio = [
-#     { :when => lambda {|jobs| jobs < 15 }, :workers => 1 },
-#     { :when => lambda {|jobs| jobs < 35 }, :workers => 2 },
-#     { :when => lambda {|jobs| jobs < 60 }, :workers => 3 },
-#     { :when => lambda {|jobs| jobs < 80 }, :workers => 4 }
-#   ]
-# end
+HireFire::Resource.configure do |config|
+  config.dyno(:worker) do
+    HireFire::Macro::Delayed::Job.queue
+  end
+end

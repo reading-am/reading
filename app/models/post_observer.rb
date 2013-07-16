a9246258391ac2256d63392e5c5fe9a3eea050cf
Greg Leppert <greg@formasfunction.com>
2013-07-16 13:39:57 -0400

    Merging use-built-in-methods-for-serialization

--- app/models/post_observer.rb
+++ app/models/post_observer.rb
@@ -1,16 +1,16 @@
 class PostObserver < ActiveRecord::Observer
 
   def after_create post
-    post.user.hooks.each do |hook| hook.run(post, :new) end
+    post.user.hooks.each do |hook| hook.run(post, 'new') end
     if !post.yn.nil?
-      post.user.hooks.each do |hook| hook.run(post, post.yn ? :yep : :nope) end
+      post.user.hooks.each do |hook| hook.run(post, post.yn ? 'yep' : 'nope') end
     end
     Broadcaster::signal :create, post
   end
 
   def after_update post
     if post.yn_changed? and !post.yn.nil?
-      post.user.hooks.each do |hook| hook.run(post, post.yn ? :yep : :nope) end
+      post.user.hooks.each do |hook| hook.run(post, post.yn ? 'yep' : 'nope') end
     end
     Broadcaster::signal :update, post
   end

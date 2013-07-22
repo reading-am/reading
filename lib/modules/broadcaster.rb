9f4f4ffa04efb06354d96e5d506a5b48b3e079aa
Greg Leppert <greg@formasfunction.com>
2013-07-21 21:35:46 -0400

    Clean up Broadcaster

--- lib/modules/broadcaster.rb
+++ lib/modules/broadcaster.rb
@@ -2,14 +2,14 @@ module Broadcaster
 
   def self.signal action, obj
     async = defined? PUSHER_QUEUE # this won't exist in the console
-    simple_obj = obj.simple_obj
+    msg = {action: action.to_s, msg: obj.simple_obj}
 
     obj.channels.each_slice(100).to_a.each do |channels| # Pusher can only take channels in groups of 100 or less
-      msg = {:channels => channels, :action => action.to_s, :msg => simple_obj}
+      msg[:channels] = channels
       if async
         PUSHER_QUEUE << msg
       else
-        Pusher.trigger msg[:channels], msg[:action].to_s, msg[:msg]
+        Pusher.trigger msg[:channels], msg[:action], msg[:msg]
       end
     end
   end

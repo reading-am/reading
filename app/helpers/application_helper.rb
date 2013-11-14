5f9105e740851ee10fdfaa0b7ae8afd978755cfb
Greg Leppert <greg@formasfunction.com>
2013-11-14 12:54:42 -0500

    Make sure to grab the newest manifest file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -75,7 +75,7 @@ module ApplicationHelper
 
   def js_manifest
     manifest = {}
-    path = Dir[File.join("#{Rails.root}/public/assets", "rjs-manifest*.json")].first
+    path = Dir[File.join("#{Rails.root}/public/assets", "rjs-manifest*.json")].max { |a,b| test(?M, a) <=> test(?M, b) }
     json = ActiveSupport::JSON.decode(File.new(path)) rescue {'files' => {}, 'assets' => {}}
     json['assets'].each {|key, val| manifest[key[0..-4]] = val[0..-4] }
     manifest.to_json.html_safe

574f1187089658c82f44c3f686a8d8e8171804f3
Greg Leppert <greg@leppert.me>
2014-03-14 18:23:31 -0400

    Merge switch-to-www

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def bookmarklet_url user
-    "javascript:(function(){window.reading={token:'#{user.token}',platform:'bookmarklet',version:'#{BOOKMARKLET_VERSION}'};var head=document.getElementsByTagName('head')[0],script=document.createElement('script');script.src='#{Rails.env == 'production' ? 'https' : 'http'}://#{DOMAIN}/assets/bookmarklet/loader.js';head.appendChild(script);})()"
+    "javascript:(function(){window.reading={token:'#{user.token}',platform:'bookmarklet',version:'#{BOOKMARKLET_VERSION}'};var head=document.getElementsByTagName('head')[0],script=document.createElement('script');script.src='#{ROOT_URL}/assets/bookmarklet/loader.js';head.appendChild(script);})()"
   end
 
   def mustache_helpers

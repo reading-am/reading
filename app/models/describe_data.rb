352fbe8ea4a78dc1c335203df644b3bbdee9aaf8
Greg Leppert <greg@leppert.me>
2014-03-22 13:29:24 -0400

    Add validation and parse

--- app/models/describe_data.rb
+++ app/models/describe_data.rb
@@ -2,17 +2,35 @@ class DescribeData < ActiveRecord::Base
   serialize :response, JSON
   belongs_to :page
 
-  validates_presence_of :page
-
-  before_create { fetch if response.blank? }
+  validates_presence_of :page, :response
+  before_validation { fetch if new_record? and response.blank? }
 
   def fetch
-    c = Curl::Easy.new
-    c.follow_location = true
-    c.url = "http://#{ENV['DESCRIBE_HOST']}:#{ENV['DESCRIBE_PORT']}?" + {url: page.url, format: "json"}.to_query
+    c = Curl::Easy.new "http://#{ENV['DESCRIBE_HOST']}:#{ENV['DESCRIBE_PORT']}?" +
+        {url: page.url, format: "json"}.to_query
+    perform c
+  end
+
+  def parse html
+    file = Curl::PostField.content('file', html)
+    file.remote_file = 'page.html'
+    file.content_type = 'application/octet-stream'
+
+    c = Curl::Easy.http_post("http://#{ENV['DESCRIBE_HOST']}:#{ENV['DESCRIBE_PORT']}",
+                             Curl::PostField.content('format', 'json'),
+                             Curl::PostField.content('url', page.url),
+                             file)
+    c.multipart_form_post = true
+    perform c
+  end
+
+  private
+
+  def perform curl
+    curl.follow_location = true
     begin
-      c.perform
-      obj = ActiveSupport::JSON.decode c.body_str
+      curl.perform
+      obj = ActiveSupport::JSON.decode curl.body_str
       self.response = obj["response"] unless obj.blank? or obj["error"]
     rescue Exception => e
       puts e

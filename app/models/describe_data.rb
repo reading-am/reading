011c3d0d052b9f084022d6f982477c721271c2e1
Greg Leppert <greg@leppert.me>
2014-03-23 11:29:05 -0400

    Switch to Typhoeus for all describe requests

--- app/models/describe_data.rb
+++ app/models/describe_data.rb
@@ -5,47 +5,33 @@ class DescribeData < ActiveRecord::Base
   validates_presence_of :page, :response
   before_validation { fetch if new_record? and response.blank? }
 
+  ENDPOINT = "http://#{ENV['DESCRIBE_HOST']}:#{ENV['DESCRIBE_PORT']}"
+
   def fetch
-    c = Curl::Easy.new "http://#{ENV['DESCRIBE_HOST']}:#{ENV['DESCRIBE_PORT']}?" +
-        {url: page.url, format: "json"}.to_query
-    perform c
+    request = Typhoeus::Request.new(ENDPOINT, params: {
+      url: page.url, format: "json"
+    })
+    run request
   end
 
   def parse html
     temp = Tempfile.new("page.html")
     temp.write html
-    begin
-      resp = Typhoeus.post "http://#{ENV['DESCRIBE_HOST']}:#{ENV['DESCRIBE_PORT']}",
-                           body: { format: 'json', url: page.url, file: File.open(temp.path, 'r') }
-      obj = ActiveSupport::JSON.decode resp.body
-      self.response = obj["response"] unless obj.blank? or obj["error"]
-    rescue Exception => e
-      puts e
-      nil
-    ensure
-      temp.close!
-    end
-
-    ## This works for curb / curl but multipart_form_post = true
-    ## causes a segfault
-    # file = Curl::PostField.content('file', html)
-    # file.remote_file = 'page.html'
-    # file.content_type = 'application/octet-stream'
-    # c = Curl::Easy.new("http://#{ENV['DESCRIBE_HOST']}:#{ENV['DESCRIBE_PORT']}")
-    # c.multipart_form_post = true
-    # c.http_post(Curl::PostField.content('format', 'json'),
-    #             Curl::PostField.content('url', page.url),
-    #             file)
-    # perform c
+    request = Typhoeus::Request.new(ENDPOINT, method: :post, body: {
+      format: 'json',
+      url: page.url,
+      file: File.open(temp.path, 'r')
+    })
+    run request
+    temp.close!
   end
 
   private
 
-  def perform curl
-    curl.follow_location = true
+  def run request
     begin
-      curl.perform
-      obj = ActiveSupport::JSON.decode curl.body_str
+      response = request.run
+      obj = ActiveSupport::JSON.decode response.body
       self.response = obj["response"] unless obj.blank? or obj["error"]
     rescue Exception => e
       puts e

3f3c53d5e704d249cbf49ff3afa6111abec97c6a
Greg Leppert <greg@leppert.me>
2014-08-15 19:22:13 -0700

    Merging add-banning

--- Procfile
+++ Procfile
@@ -2,6 +2,6 @@ web: bundle exec puma -p $PORT
 worker: bundle exec rake jobs:work
 search: bundle exec rake sunspot:solr:run
 memcached: memcached -v
-devmail: mailcatcher --foreground
+devmail: ruby -rbundler/setup -e "Bundler.clean_exec('mailcatcher --foreground')"
 testrubyserver: bundle exec spork
 testruby: rake watchr
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -69,7 +69,9 @@ class ApplicationController < ActionController::Base
 
   def check_signed_in
     if signed_in?
-      if (controller_path == 'users' or (controller_path == 'registrations' and action_name == 'edit') or (controller_path == 'posts' and action_name == 'index')) and (current_user.username.blank? or current_user.email.blank? or !current_user.has_pass?)
+      if current_user.suspended? and !(controller_path == 'users' and action_name == 'suspended')
+        redirect_to '/support/suspended'
+      elsif (controller_path == 'users' or (controller_path == 'registrations' and action_name == 'edit') or (controller_path == 'posts' and action_name == 'index')) and (current_user.username.blank? or current_user.email.blank? or !current_user.has_pass?)
         redirect_to '/almost_ready'
       elsif request.path_info == '/'
         redirect_to "/#{current_user.username}/list"

c24ff6591ac6ed48ff93e4765894e54475b1a672
Greg Leppert <greg@leppert.me>
2014-09-05 19:36:11 -0700

    Upgrade pg and change db config settings

--- config/puma.rb
+++ config/puma.rb
@@ -5,12 +5,3 @@ max_threads = Integer(ENV['MAX_THREADS'] || 12)
  
 threads min_threads, max_threads
 workers Integer(ENV['WORKER_COUNT'] || 2)
- 
-on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    config = Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 #seconds
-    config['pool']              = ENV['DB_POOL'] || 5
-    ActiveRecord::Base.establish_connection(config)
-  end
-end

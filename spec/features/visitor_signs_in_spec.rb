c9e51636a6f7dc3e7639df29fdb2d10b719ca29f
Greg Leppert <greg@leppert.me>
2015-01-25 15:12:18 -0500

    Add additional tests for signing in

--- spec/features/visitor_signs_in_spec.rb
+++ spec/features/visitor_signs_in_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Signing in' do
   fixtures :users
 
-  scenario 'Signing in with correct credentials' do
+  scenario 'Signing in with correct username credentials' do
     user = users(:greg)
 
     visit '/sign_in'
@@ -13,5 +13,30 @@ feature 'Signing in' do
     end
     click_button 'Sign in'
     expect(current_path).to eq("/#{user.username}/list")
+  end
+
+  scenario 'Signing in with correct email credentials' do
+    user = users(:greg)
+
+    visit '/sign_in'
+    within('#signin_form_wrapper') do
+      fill_in 'Username or email', with: user.email
+      fill_in 'Password', with: 'testingtesting'
+    end
+    click_button 'Sign in'
+    expect(current_path).to eq("/#{user.username}/list")
+  end
+
+  scenario 'Signing in with incorrect credentials' do
+    user = users(:greg)
+
+    visit '/sign_in'
+    within('#signin_form_wrapper') do
+      fill_in 'Username or email', with: user.username
+      fill_in 'Password', with: 'incorrectpassword'
+    end
+    click_button 'Sign in'
+    expect(current_path).to eq('/sign_in')
+    expect(page).to have_content 'Invalid'
   end
 end

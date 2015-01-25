049cc9e29e73a3add2655373f92d6e996bbec717
Greg Leppert <greg@leppert.me>
2015-01-25 10:44:18 -0500

    Revise sign in feature test

--- spec/features/visitor_signs_in_spec.rb
+++ spec/features/visitor_signs_in_spec.rb
@@ -1,29 +1,17 @@
 require 'rails_helper'
 
-feature "Signing in" do
-  background do
-    User.make(:email => 'user@example.com', :password => 'caplin')
-  end
+feature 'Signing in' do
+  fixtures :users
 
-  scenario "Signing in with correct credentials" do
-    visit '/sessions/new'
-    within("#session") do
-      fill_in 'Email', :with => 'user@example.com'
-      fill_in 'Password', :with => 'caplin'
+  scenario 'Signing in with correct credentials' do
+    user = users(:greg)
+
+    visit '/sign_in'
+    within('#signin_form_wrapper') do
+      fill_in 'Username or email', with: user.username
+      fill_in 'Password', with: 'testingtesting'
     end
     click_button 'Sign in'
-    expect(page).to have_content 'Success'
-  end
-
-  given(:other_user) { User.make(:email => 'other@example.com', :password => 'rous') }
-
-  scenario "Signing in as another user" do
-    visit '/sessions/new'
-    within("#session") do
-      fill_in 'Email', :with => other_user.email
-      fill_in 'Password', :with => other_user.password
-    end
-    click_button 'Sign in'
-    expect(page).to have_content 'Invalid email or password'
+    expect(current_path).to eq("/#{user.username}/list")
   end
 end

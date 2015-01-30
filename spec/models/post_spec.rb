b43b9d10cc997472d9d6d86d487555feeea31815
Greg Leppert <greg@leppert.me>
2015-01-29 22:33:08 -0500

    Fix rspec syntax

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,7 @@ describe Post do
   context "when new" do
     it "won't save without a valid page_id" do
       post = Post.create
-      post.should be_an_instance_of Post
+      expect(post).to be_an_instance_of Post
     end
   end
 end

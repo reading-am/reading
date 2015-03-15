17fc426551316fbad8185e52a05ef950c68adbb6
Greg Leppert <greg@leppert.me>
2015-03-15 11:00:56 -0400

    Revise API tests

--- spec/support/matchers/api_schema_matcher.rb
+++ spec/support/matchers/api_schema_matcher.rb
@@ -3,6 +3,6 @@ RSpec::Matchers.define :match_response_schema do |schema|
     module_path = response.request.env["action_controller.instance"].class.name.deconstantize.underscore
     schema_directory = "#{Dir.pwd}/spec/schemas/#{module_path}"
     schema_path = "#{schema_directory}/#{schema}.json"
-    JSON::Validator.validate!(schema_path, response.body, strict: true)
+    JSON::Validator.validate!(schema_path, response.body, strict: true, list: response.body[0] == '[')
   end
 end

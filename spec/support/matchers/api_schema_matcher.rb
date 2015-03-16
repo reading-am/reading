74c475d6914f93b2abb16aff5df49f7c0aac1241
Greg Leppert <greg@leppert.me>
2015-03-16 12:16:40 -0400

    Clear json validation cache with each call

--- spec/support/matchers/api_schema_matcher.rb
+++ spec/support/matchers/api_schema_matcher.rb
@@ -4,6 +4,11 @@ RSpec::Matchers.define :match_response_schema do |schema|
     schema_directory = "#{Dir.pwd}/spec/schemas/#{module_path}"
     schema_path = "#{schema_directory}/#{schema}.json"
     json = JSON.parse(response.body)
+
+    # json-schema will cache the schema with the list: (true|false) value
+    # so if you don't clear the cache manually (clear_cache param won't work)
+    # it'll always have the list format from the first call
+    JSON::Validator.clear_cache
     JSON::Validator.validate!(schema_path, json, strict: true, list: json.is_a?(Array))
   end
 end

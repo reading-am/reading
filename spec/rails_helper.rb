bf04b476c7441a42f1c2b5ed57dd286cdf6525c4
Greg Leppert <greg@leppert.me>
2015-03-22 15:23:12 -0400

    Use rack-test for api

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
-require 'features_helper'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -54,9 +54,6 @@ RSpec.configure do |config|
   require 'devise'
   Devise.stretches = 1
   config.include Devise::TestHelpers, type: :controller
-
-  # http://makandracards.com/makandra/17775-rspec-where-to-put-custom-matchers-and-other-support-code
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   config.before(:all) do
     self.class.set_fixture_class oauth_access_tokens: Doorkeeper::AccessToken,
---
+++ spec/support/api_helper.rb
@@ -0,0 +1,29 @@
+module Rack
+  class MockResponse
+    attr_accessor :request
+  end
+end
+
+# via: https://gist.github.com/alex-zige/5795358
+module ApiHelper
+  include Rack::Test::Methods
+
+  def app
+    Rails.application
+  end
+
+  def response
+    last_response.request = last_request
+    last_response
+  end
+
+  def set_auth_header(t=nil)
+    header 'Authorization', "Bearer #{(t || token).token}"
+  end
+
+  def url_base
+    '/api/v1'
+  end
+end
+
+RSpec.configure { |c| c.include ApiHelper, type: :api }

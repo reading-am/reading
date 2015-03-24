0a7bf1afbf48b512feb631e23bb613531dc0a367
Greg Leppert <greg@leppert.me>
2015-03-24 00:59:12 -0400

    Handle empty lists in json schema validation

--- spec/support/matchers/api_schema_matcher.rb
+++ spec/support/matchers/api_schema_matcher.rb
@@ -4,11 +4,16 @@ RSpec::Matchers.define :match_response_schema do |schema|
     schema_directory = "#{Dir.pwd}/spec/schemas/#{module_path}"
     schema_path = "#{schema_directory}/#{schema}.json"
     json = JSON.parse(response.body)
+    is_list = json.is_a?(Array)
+
+    if is_list && json.length == 0
+      raise StandardError.new("Received an empty list but that's probably not what you were expecting.")
+    end
 
     # json-schema will cache the schema with the list: (true|false) value
     # so if you don't clear the cache manually (clear_cache param won't work)
     # it'll always have the list format from the first call
     JSON::Validator.clear_cache
-    JSON::Validator.validate!(schema_path, json, strict: true, list: json.is_a?(Array))
+    JSON::Validator.validate!(schema_path, json, strict: true, list: is_list)
   end
 end

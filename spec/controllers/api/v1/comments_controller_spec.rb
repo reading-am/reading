5acd420d33f3c0a54f4150399cc58422bcc9cd37
Greg Leppert <greg@leppert.me>
2015-03-24 00:59:46 -0400

    Example tests for comments and posts

--- spec/controllers/api/v1/comments_controller_spec.rb
+++ spec/controllers/api/v1/comments_controller_spec.rb
@@ -2,55 +2,78 @@ require 'rails_helper'
 
 describe Api::V1::CommentsController, type: :api do
   include_context 'api defaults'
-  fixtures :pages, :comments
+  fixtures :users, :pages, :comments
 
   let(:resource) { comments(:single_mention) }
 
-  describe 'index' do
-    it_behaves_like 'a restricted endpoint', 'public'
-    it_behaves_like 'a successful request'
-    it_behaves_like 'a response that renders JSON'
-  end
+  describe '/comments' do
 
-  describe 'show' do
-    let(:endpoint) { detail_endpoint }
-    it_behaves_like 'a restricted endpoint', 'public'
-    it_behaves_like 'a successful request'
-    it_behaves_like 'a response that renders JSON'
-  end
-
-  describe 'create' do
-    let(:method) { :post }
-    let(:params) do
-      { model: { body: 'This is a test comment',
-                 page_id: pages(:daringfireball).id } }
+    describe 'index' do
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
     end
-    it_behaves_like 'a restricted endpoint', 'write'
-    it_behaves_like 'a successful request'
-    it_behaves_like 'a response that renders JSON'
+
+    describe 'show' do
+      let(:endpoint) { detail_endpoint }
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    describe 'create' do
+      let(:method) { :post }
+      let(:params) do
+        { model: { body: 'This is a test comment',
+                   page_id: pages(:daringfireball).id } }
+      end
+      it_behaves_like 'a restricted endpoint', 'write'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    describe 'update' do
+      let(:method) { :patch }
+      let(:endpoint) { detail_endpoint }
+      let(:params) { { model: { body: 'This is an updated comment' } } }
+      it_behaves_like 'a restricted endpoint', 'write'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    describe 'delete' do
+      let(:method) { :delete }
+      let(:endpoint) { detail_endpoint }
+      it_behaves_like 'a restricted endpoint', 'write'
+      it_behaves_like 'a successful request'
+    end
+
+    describe 'stats' do
+      let(:schema) { 'shared/stats' }
+      let(:endpoint) { "#{list_endpoint}/stats" }
+      it_behaves_like 'a restricted endpoint', 'admin'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
   end
 
-  describe 'update' do
-    let(:method) { :patch }
-    let(:endpoint) { detail_endpoint }
-    let(:params) { { model: { body: 'This is an updated comment' } } }
-    it_behaves_like 'a restricted endpoint', 'write'
-    it_behaves_like 'a successful request'
-    it_behaves_like 'a response that renders JSON'
+  describe '/users/:id/comments' do
+    let(:list_endpoint) { "#{url_base}/users/#{users(:greg).id}/comments" }
+
+    describe 'index' do
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
   end
 
-  describe 'delete' do
-    let(:method) { :delete }
-    let(:endpoint) { detail_endpoint }
-    it_behaves_like 'a restricted endpoint', 'write'
-    it_behaves_like 'a successful request'
-  end
+  describe '/pages/:id/comments' do
+    let(:list_endpoint) { "#{url_base}/pages/#{pages(:daringfireball).id}/comments" }
 
-  describe 'stats' do
-    let(:schema) { 'shared/stats' }
-    let(:endpoint) { "#{list_endpoint}/stats" }
-    it_behaves_like 'a restricted endpoint', 'admin'
-    it_behaves_like 'a successful request'
-    it_behaves_like 'a response that renders JSON'
+    describe 'index' do
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
   end
 end
--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -1,28 +1,118 @@
 require 'rails_helper'
 
-describe Api::V1::PostsController do
-  render_views
-  fixtures :users, :posts
+describe Api::V1::PostsController, type: :api do
+  include_context 'api defaults'
+  fixtures :users, :domains, :pages, :posts
 
-  describe 'GET index' do
-    it 'returns a JSON array of posts' do
-      get :index,
-          format: :json
+  let(:resource) { posts(:one) }
 
-      expect(response).to have_http_status(:success)
-      json = JSON.parse(response.body)['posts']
-      expect(json).not_to be_empty
+  shared_context 'a medium filter' do
+    Page::MEDIUMS.each do |medium|
+      describe "/#{medium}" do
+        let(:list_endpoint) { "#{super()}/#{medium}" }
+        it_behaves_like 'a restricted endpoint', 'public'
+        it_behaves_like 'a successful request'
+        it_behaves_like 'a response that renders JSON'
+      end
     end
   end
 
-  describe 'GET show' do
-    it 'returns a JSON object of a post' do
-      get :show,
-          format: :json,
-          id: posts(:one).id
+  describe '/posts' do
 
-      expect(response).to have_http_status(:success)
-      expect(response).to match_response_schema('posts/post')
+    describe 'index' do
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    it_behaves_like 'a medium filter'
+
+    describe 'show' do
+      let(:endpoint) { detail_endpoint }
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    describe 'create' do
+      let(:method) { :post }
+      let(:params) do
+        { model: { body: 'This is a test comment',
+                   page_id: pages(:daringfireball).id } }
+      end
+      it_behaves_like 'a restricted endpoint', 'write'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    describe 'update' do
+      let(:method) { :patch }
+      let(:endpoint) { detail_endpoint }
+      let(:params) { { model: { yn: true } } }
+      it_behaves_like 'a restricted endpoint', 'write'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    describe 'delete' do
+      let(:method) { :delete }
+      let(:endpoint) { detail_endpoint }
+      it_behaves_like 'a restricted endpoint', 'write'
+      it_behaves_like 'a successful request'
+    end
+
+    describe 'stats' do
+      let(:schema) { 'shared/stats' }
+      let(:endpoint) { "#{list_endpoint}/stats" }
+      it_behaves_like 'a restricted endpoint', 'admin'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+  end
+
+  describe '/users/:id/posts' do
+    let(:list_endpoint) { "#{url_base}/users/#{users(:greg).id}/posts" }
+
+    describe 'index' do
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    it_behaves_like 'a medium filter'
+  end
+
+  describe '/users/:id/following/posts' do
+    let(:list_endpoint) { "#{url_base}/users/#{users(:greg).id}/following/posts" }
+
+    describe 'index' do
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    it_behaves_like 'a medium filter'
+  end
+
+  describe '/domains/:id/posts' do
+    let(:list_endpoint) { "#{url_base}/domains/#{domains(:daringfireball).id}/posts" }
+
+    describe 'index' do
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
+    end
+
+    it_behaves_like 'a medium filter'
+  end
+
+  describe '/pages/:id/posts' do
+    let(:list_endpoint) { "#{url_base}/pages/#{pages(:daringfireball).id}/posts" }
+
+    describe 'index' do
+      it_behaves_like 'a restricted endpoint', 'public'
+      it_behaves_like 'a successful request'
+      it_behaves_like 'a response that renders JSON'
     end
   end
 end

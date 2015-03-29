54f7c895c0b3430068efb3dae70b3e97ceddf686
Greg Leppert <greg@leppert.me>
2015-03-29 13:56:56 -0400

    Create a shared context for yep nope buttons

--- spec/features/user_interacts_with_feed_spec.rb
+++ spec/features/user_interacts_with_feed_spec.rb
@@ -130,39 +130,45 @@ feature "User's feed", js: true do
     expect(Post.count).to eq(db_count - 1), "Post wasn't removed from the database"
   end
 
-  scenario 'clicking a yep button toggles yep on a post' do
-    login_as users(:greg), scope: :user
-    db_count = Post.where(yn: true).count
+  shared_context 'a yep nope button' do |yn|
+    scenario "clicking a #{yn} button toggles #{yn} on a post" do
+      login_as users(:greg), scope: :user
+      query = Post.where(yn: yn.to_s == 'yep')
+      db_count = query.count
 
-    visit '/'
-    scroll_to_bottom
+      visit '/'
+      scroll_to_bottom
 
-    row = nil
-    all('.pa_destroy').each do |el|
-      row = first_row_containing(el)
-      if row.all('.pa_yep.r_active').count == 0
-        break
-      else
-        row = nil
+      row = nil
+      all('.pa_destroy').each do |el|
+        row = first_row_containing(el)
+        if row.all(".pa_#{yn}.r_active").count == 0
+          break
+        else
+          row = nil
+        end
+      end
+
+      expect(row).to be_truthy, "A posted page that didn't already have #{yn} wasn't found"
+
+      within(row) do
+        dom_count = all(".r_subpost .r_#{yn}").count
+        
+        find(".pa_#{yn}").click
+        wait_for_js
+        expect(page).to have_selector(".pa_#{yn}.r_active"), "#{yn} button didn't change state"
+        expect(page).to have_selector(".r_subpost .r_#{yn}", count: dom_count + 1), "A post wasn't newly marked as '#{yn}'"
+        expect(query.count).to eq(db_count + 1), "Post wasn't maked as '#{yn}' in the database"
+
+        find(".pa_#{yn}").click
+        wait_for_js
+        expect(page).not_to have_selector(".pa_#{yn}.r_active"), "#{yn} button didn't change state"
+        expect(page).to have_selector(".r_subpost .r_#{yn}", count: dom_count), "'#{yn}' wasn't removed from the subpost DOM"
+        expect(query.count).to eq(db_count), "'#{yn}' wasn't removed from the post in the database"
       end
     end
-
-    expect(row).to be_truthy, "A posted page that wasn't already yepped wasn't found"
-
-    within(row) do
-      dom_count = all('.r_subpost .r_yep').count
-      
-      find('.pa_yep').click
-      wait_for_js
-      expect(page).to have_selector('.pa_yep.r_active'), "Yep button didn't change state"
-      expect(page).to have_selector('.r_subpost .r_yep', count: dom_count + 1), "A post wasn't newly marked as 'yep'"
-      expect(Post.where(yn: true).count).to eq(db_count + 1), "Post wasn't maked as yepped in the database"
-
-      find('.pa_yep').click
-      wait_for_js
-      expect(page).not_to have_selector('.pa_yep.r_active'), "Yep button didn't change state"
-      expect(page).to have_selector('.r_subpost .r_yep', count: dom_count), "'Yep' wasn't removed from the subpost DOM"
-      expect(Post.where(yn: true).count).to eq(db_count), "'Yep' wasn't removed from the post in the database"
-    end
   end
+
+  it_behaves_like 'a yep nope button', :yep
+  it_behaves_like 'a yep nope button', :nope
 end

df6036f410b6e4fd5f8fe15f36440af6418090dc
Greg Leppert <greg@leppert.me>
2015-03-30 18:29:18 -0400

    Group tests and make filtering tests more resilient

--- spec/features/user_interacts_with_feed_spec.rb
+++ spec/features/user_interacts_with_feed_spec.rb
@@ -38,144 +38,120 @@ feature "User's feed", js: true do
     expect(all('.r_subpost').count).to be > limit, "Additional page rows weren't added after scroll"
   end
 
-  scenario 'clicking the Post a Link button allows a new post to be created' do
-    login_as users(:greg), scope: :user
+  describe 'header' do
 
-    visit '/'
-    db_count = Post.count
-    dom_count = all('.page_row').count
-    
-    click_link 'Post a Link'
-    wait_for_js
-    fill_in "Enter the url you'd like to post", with: 'http://example.com'
-    click_button 'Add Post'
-    wait_for_js
+    scenario 'clicking the Post a Link button allows a new post to be created' do
+      login_as users(:greg), scope: :user
 
-    expect(page).not_to have_selector('#new_post_row'), "The new post input wasn't hidden"
-    expect(all('.page_row').count).to eq(dom_count + 1), "A new page wasn't added to the DOM"
-    expect(Post.count).to eq(db_count + 1), "A new post wasn't added to the database"
-  end
+      visit '/'
+      db_count = Post.count
+      dom_count = all('.page_row').count
 
-  scenario 'clicking a post icon toggles between comments and posts' do
-    login_as users(:greg), scope: :user
-
-    visit '/'
-    scroll_to_bottom
-    within(first_row_containing('.has_comments')) do
-      button = find('.comments_icon')
-      count = button.text
-      button.click
+      click_link 'Post a Link'
       wait_for_js
-      expect(find('.r_comments')).to have_selector('.r_comment', count: count)
-
-      button = find('.posts_icon')
-      count = button.text
-      button.click
+      fill_in "Enter the url you'd like to post", with: 'http://example.com'
+      click_button 'Add Post'
       wait_for_js
-      expect(find('.r_subposts')).to have_selector('.r_subpost', count: count)
+
+      expect(page).not_to have_selector('#new_post_row'), "The new post input wasn't hidden"
+      expect(all('.page_row').count).to eq(dom_count + 1), "A new page wasn't added to the DOM"
+      expect(Post.count).to eq(db_count + 1), "A new post wasn't added to the database"
+    end
+
+    scenario 'selecting a medium filters the posts' do
+      login_as users(:greg), scope: :user
+
+      visit '/'
+
+      dom_count = all('.page_row').count
+      expect(dom_count).to be > 0, "No posts were rendered"
+
+      select 'images', from: 'medium'
+      wait_for_js
+
+      expect(all('.page_row').count).to be > 0, "No posts were rendered"
+      expect(all('.page_row').count).to be < dom_count, "A new page wasn't added to the DOM"
+    end
+
+    scenario 'selecting yep or nope filters the posts' do
+      login_as users(:greg), scope: :user
+
+      visit '/'
+
+      dom_count = all('.page_row').count
+      expect(dom_count).to be > 0, "No posts were rendered"
+
+      select 'yeps', from: 'yn'
+      wait_for_js
+
+      expect(all('.page_row').count).to be > 0, "No posts were rendered"
+      expect(all('.page_row').count).to be < dom_count, "A new page wasn't added to the DOM"
     end
   end
 
-  scenario 'clicking a comment icon displays comments' do
-    login_as users(:greg), scope: :user
-    count = Comment.count
+  describe 'page row' do
 
-    visit '/'
-    scroll_to_bottom
-    within(first_row_containing('.has_comments')) do
-      body = 'This is a comment'
-      find('.comments_icon').click
-      find_field('Add a comment')
-        .send_keys(body)
-        .send_keys(:return)
-      wait_for_js
+    scenario 'clicking a post icon toggles between comments and posts' do
+      login_as users(:greg), scope: :user
 
-      expect(first('.r_comment')).to have_text(body), "Comment wasn't added to the DOM"
-    end
+      visit '/'
+      scroll_to_bottom
+      within(first_row_containing('.has_comments')) do
+        button = find('.comments_icon')
+        count = button.text
+        button.click
+        wait_for_js
+        expect(find('.r_comments')).to have_selector('.r_comment', count: count)
 
-    expect(Comment.count).to eq(count + 1), "Comment wasn't added to the database"
-  end
-
-  scenario 'clicking a post button posts a page' do
-    login_as users(:greg), scope: :user
-    db_count = Post.count
-
-    visit '/'
-    scroll_to_bottom
-
-    within(first_row_containing('.pa_create')) do
-      dom_count = all('.r_subpost').count
-      find('.pa_create').click
-      wait_for_js
-      expect(page).to have_selector('.pa_destroy'), "Post button didn't change state"
-      expect(page).to have_selector('.r_subpost', count: dom_count + 1)
-    end
-
-    expect(Post.count).to eq(db_count + 1), "Post wasn't added to the database"
-  end
-
-  scenario 'clicking a post delete button deletes a post' do
-    login_as users(:greg), scope: :user
-    db_count = Post.count
-
-    visit '/'
-    scroll_to_bottom
-
-    row = nil
-    all('.pa_destroy').each do |el|
-      row = first_row_containing(el)
-      if row.all('.r_subpost').count > 1
-        break
-      else
-        row = nil
+        button = find('.posts_icon')
+        count = button.text
+        button.click
+        wait_for_js
+        expect(find('.r_subposts')).to have_selector('.r_subpost', count: count)
       end
     end
 
-    expect(row).to be_truthy, "A page with more than one subpost wasn't found"
-
-    within(row) do
-      dom_count = all('.r_subpost').count
-      find('.pa_destroy').click
-      page.driver.browser.switch_to.alert.accept
-      wait_for_js
-      expect(page).to have_selector('.pa_create'), "Post button didn't change state"
-      expect(page).to have_selector('.r_subpost', count: dom_count - 1)
-    end
-
-    expect(Post.count).to eq(db_count - 1), "Post wasn't removed from the database"
-  end
-
-  scenario 'deleting the last post removes the page from the DOM' do
-    login_as users(:greg), scope: :user
-    db_count = Post.count
-
-    visit '/'
-    scroll_to_bottom
-    dom_count = all('.page_row').count
-
-    row = nil
-    all('.pa_destroy').each do |el|
-      row = first_row_containing(el)
-      break if row.all('.r_subpost').count == 1
-    end
-
-    expect(row).to be_truthy, "A page with only one subpost wasn't found"
-
-    within(row) do
-      find('.pa_destroy').click
-      page.driver.browser.switch_to.alert.accept
-      wait_for_js
-    end
-
-    expect(all('.page_row').count).to eq(dom_count - 1), "Page wasn't removed from the DOM"
-    expect(Post.count).to eq(db_count - 1), "Post wasn't removed from the database"
-  end
-
-  shared_context 'a yep nope button' do |yn|
-    scenario "clicking a #{yn} button toggles #{yn} on a post" do
+    scenario 'clicking a comment icon displays comments' do
       login_as users(:greg), scope: :user
-      query = Post.where(yn: yn.to_s == 'yep')
-      db_count = query.count
+      count = Comment.count
+
+      visit '/'
+      scroll_to_bottom
+      within(first_row_containing('.has_comments')) do
+        body = 'This is a comment'
+        find('.comments_icon').click
+        find_field('Add a comment')
+          .send_keys(body)
+          .send_keys(:return)
+        wait_for_js
+
+        expect(first('.r_comment')).to have_text(body), "Comment wasn't added to the DOM"
+      end
+
+      expect(Comment.count).to eq(count + 1), "Comment wasn't added to the database"
+    end
+
+    scenario 'clicking a post button posts a page' do
+      login_as users(:greg), scope: :user
+      db_count = Post.count
+
+      visit '/'
+      scroll_to_bottom
+
+      within(first_row_containing('.pa_create')) do
+        dom_count = all('.r_subpost').count
+        find('.pa_create').click
+        wait_for_js
+        expect(page).to have_selector('.pa_destroy'), "Post button didn't change state"
+        expect(page).to have_selector('.r_subpost', count: dom_count + 1)
+      end
+
+      expect(Post.count).to eq(db_count + 1), "Post wasn't added to the database"
+    end
+
+    scenario 'clicking a post delete button deletes a post' do
+      login_as users(:greg), scope: :user
+      db_count = Post.count
 
       visit '/'
       scroll_to_bottom
@@ -183,50 +159,110 @@ feature "User's feed", js: true do
       row = nil
       all('.pa_destroy').each do |el|
         row = first_row_containing(el)
-        if row.all(".pa_#{yn}.r_active").count == 0
+        if row.all('.r_subpost').count > 1
           break
         else
           row = nil
         end
       end
 
-      expect(row).to be_truthy, "A posted page that didn't already have #{yn} wasn't found"
+      expect(row).to be_truthy, "A page with more than one subpost wasn't found"
 
       within(row) do
-        dom_count = all(".r_subpost .r_#{yn}").count
-        
-        find(".pa_#{yn}").click
+        dom_count = all('.r_subpost').count
+        find('.pa_destroy').click
+        page.driver.browser.switch_to.alert.accept
         wait_for_js
-        expect(page).to have_selector(".pa_#{yn}.r_active"), "#{yn} button didn't change state"
-        expect(page).to have_selector(".r_subpost .r_#{yn}", count: dom_count + 1), "A post wasn't newly marked as '#{yn}'"
-        expect(query.count).to eq(db_count + 1), "Post wasn't maked as '#{yn}' in the database"
+        expect(page).to have_selector('.pa_create'), "Post button didn't change state"
+        expect(page).to have_selector('.r_subpost', count: dom_count - 1)
+      end
 
-        find(".pa_#{yn}").click
+      expect(Post.count).to eq(db_count - 1), "Post wasn't removed from the database"
+    end
+
+    scenario 'deleting the last post removes the page from the DOM' do
+      login_as users(:greg), scope: :user
+      db_count = Post.count
+
+      visit '/'
+      scroll_to_bottom
+      dom_count = all('.page_row').count
+
+      row = nil
+      all('.pa_destroy').each do |el|
+        row = first_row_containing(el)
+        break if row.all('.r_subpost').count == 1
+      end
+
+      expect(row).to be_truthy, "A page with only one subpost wasn't found"
+
+      within(row) do
+        find('.pa_destroy').click
+        page.driver.browser.switch_to.alert.accept
         wait_for_js
-        expect(page).not_to have_selector(".pa_#{yn}.r_active"), "#{yn} button didn't change state"
-        expect(page).to have_selector(".r_subpost .r_#{yn}", count: dom_count), "'#{yn}' wasn't removed from the subpost DOM"
-        expect(query.count).to eq(db_count), "'#{yn}' wasn't removed from the post in the database"
+      end
+
+      expect(all('.page_row').count).to eq(dom_count - 1), "Page wasn't removed from the DOM"
+      expect(Post.count).to eq(db_count - 1), "Post wasn't removed from the database"
+    end
+
+    shared_context 'a yep nope button' do |yn|
+      scenario "clicking a #{yn} button toggles #{yn} on a post" do
+        login_as users(:greg), scope: :user
+        query = Post.where(yn: yn.to_s == 'yep')
+        db_count = query.count
+
+        visit '/'
+        scroll_to_bottom
+
+        row = nil
+        all('.pa_destroy').each do |el|
+          row = first_row_containing(el)
+          if row.all(".pa_#{yn}.r_active").count == 0
+            break
+          else
+            row = nil
+          end
+        end
+
+        expect(row).to be_truthy, "A posted page that didn't already have #{yn} wasn't found"
+
+        within(row) do
+          dom_count = all(".r_subpost .r_#{yn}").count
+
+          find(".pa_#{yn}").click
+          wait_for_js
+          expect(page).to have_selector(".pa_#{yn}.r_active"), "#{yn} button didn't change state"
+          expect(page).to have_selector(".r_subpost .r_#{yn}", count: dom_count + 1), "A post wasn't newly marked as '#{yn}'"
+          expect(query.count).to eq(db_count + 1), "Post wasn't maked as '#{yn}' in the database"
+
+          find(".pa_#{yn}").click
+          wait_for_js
+          expect(page).not_to have_selector(".pa_#{yn}.r_active"), "#{yn} button didn't change state"
+          expect(page).to have_selector(".r_subpost .r_#{yn}", count: dom_count), "'#{yn}' wasn't removed from the subpost DOM"
+          expect(query.count).to eq(db_count), "'#{yn}' wasn't removed from the post in the database"
+        end
       end
     end
-  end
 
-  it_behaves_like 'a yep nope button', :yep
-  it_behaves_like 'a yep nope button', :nope
+    it_behaves_like 'a yep nope button', :yep
+    it_behaves_like 'a yep nope button', :nope
 
-  scenario "clicking a share button brings up a share sheet" do
-    login_as users(:greg), scope: :user
+    scenario "clicking a share button brings up a share sheet" do
+      login_as users(:greg), scope: :user
 
-    visit '/'
+      visit '/'
 
-    click_link('Share', match: :first)
-    expect(page).to have_selector('#r_share_menu'), "Share menu wasn't displayed"
+      click_link('Share', match: :first)
+      expect(page).to have_selector('#r_share_menu'), "Share menu wasn't displayed"
 
-    click_link('Twitter')
-    expect(windows.length).to eq(2)
+      click_link('Twitter')
+      expect(windows.length).to eq(2)
 
-    within_window(windows.last) do
-      expect(current_url).to start_with('https://twitter.com/intent')
-      current_window.close
+      within_window(windows.last) do
+        expect(current_url).to start_with('https://twitter.com/intent')
+        current_window.close
+      end
     end
   end
 end

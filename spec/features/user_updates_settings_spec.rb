b5adc32dab53dea11239f302e6f39d990946a0e7
Greg Leppert <greg@leppert.me>
2015-03-30 19:33:15 -0400

    Add a user info update test

--- spec/features/user_updates_settings_spec.rb
+++ spec/features/user_updates_settings_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 feature "User's settings", js: true do
   fixtures :users
 
+  let(:url) { '/settings/info' }
+
   describe 'delete button' do
-    let(:url) { '/settings/info' }
     let(:button_text) { 'I respectfully request to be destroyed.' }
 
     scenario 'clicking counts down and then deletes user' do
@@ -34,6 +35,25 @@ feature "User's settings", js: true do
 
       expect(page).to have_link(button_text)
       expect(User.count).to eq(db_count), "User was deleted from the database"
+    end
+  end
+
+  describe 'info form' do
+
+    scenario 'updates user info' do
+      user = users(:greg)
+      login_as user, scope: :user
+      old_name = user.name
+      new_name = 'A new name'
+      expect(new_name).not_to eq(old_name)
+
+      visit url
+      expect(page).to have_selector("input[value='#{old_name}']")
+      fill_in 'Name', with: new_name
+      click_button 'Update Me'
+
+      expect(page).to have_selector("input[value='#{new_name}']")
+      expect(user.reload.name).to eq(new_name), "The name wasn't update in the database"
     end
   end
 end

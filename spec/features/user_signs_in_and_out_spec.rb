e439eefed24b24c3e036ec8f3ed116a88dec2369
Greg Leppert <greg@leppert.me>
2015-03-31 09:43:07 -0400

    Add a sign out spec

--- spec/features/user_signs_in_and_out_spec.rb
+++ spec/features/user_signs_in_and_out_spec.rb
@@ -1,66 +1,81 @@
 require 'rails_helper'
 
-feature 'Signing in', js: true do
+feature 'User authentication', js: true do
   fixtures :users
 
-  scenario 'with correct username credentials' do
-    user = users(:greg)
+  describe 'signing in' do
 
-    visit '/sign_in'
-    within('#signin_form_wrapper') do
-      fill_in 'Username or email', with: user.username
-      fill_in 'Password', with: 'testingtesting'
+    scenario 'with correct username credentials' do
+      user = users(:greg)
+
+      visit '/sign_in'
+      within('#signin_form_wrapper') do
+        fill_in 'Username or email', with: user.username
+        fill_in 'Password', with: 'testingtesting'
+      end
+      click_button 'Sign in'
+      expect(current_path).to eq("/#{user.username}/list")
     end
-    click_button 'Sign in'
-    expect(current_path).to eq("/#{user.username}/list")
+
+    scenario 'with correct email credentials' do
+      user = users(:greg)
+
+      visit '/sign_in'
+      within('#signin_form_wrapper') do
+        fill_in 'Username or email', with: user.email
+        fill_in 'Password', with: 'testingtesting'
+      end
+      click_button 'Sign in'
+      expect(current_path).to eq("/#{user.username}/list")
+    end
+
+    scenario 'with incorrect credentials' do
+      user = users(:greg)
+
+      visit '/sign_in'
+      within('#signin_form_wrapper') do
+        fill_in 'Username or email', with: user.username
+        fill_in 'Password', with: 'incorrectpassword'
+      end
+      click_button 'Sign in'
+      expect(current_path).to eq('/sign_in')
+      expect(page).to have_content 'Invalid'
+    end
+
+    scenario 'with Twitter' do
+      visit '/'
+
+      click_link 't Twitter'
+      expect(windows.length).to eq(2)
+
+      within_window(windows.last) do
+        expect(current_url).to start_with('https://api.twitter.com/oauth/authorize')
+        current_window.close
+      end
+    end
+
+    scenario 'with Facebook' do
+      visit '/'
+
+      click_link 'f Facebook'
+      expect(windows.length).to eq(2)
+
+      within_window(windows.last) do
+        expect(current_url).to start_with('https://www.facebook.com/dialog/oauth')
+        current_window.close
+      end
+    end
   end
 
-  scenario 'with correct email credentials' do
-    user = users(:greg)
+  describe 'signing out' do
 
-    visit '/sign_in'
-    within('#signin_form_wrapper') do
-      fill_in 'Username or email', with: user.email
-      fill_in 'Password', with: 'testingtesting'
-    end
-    click_button 'Sign in'
-    expect(current_path).to eq("/#{user.username}/list")
-  end
-
-  scenario 'with incorrect credentials' do
-    user = users(:greg)
-
-    visit '/sign_in'
-    within('#signin_form_wrapper') do
-      fill_in 'Username or email', with: user.username
-      fill_in 'Password', with: 'incorrectpassword'
-    end
-    click_button 'Sign in'
-    expect(current_path).to eq('/sign_in')
-    expect(page).to have_content 'Invalid'
-  end
-
-  scenario 'with Twitter' do
-    visit '/'
-
-    click_link 't Twitter'
-    expect(windows.length).to eq(2)
-
-    within_window(windows.last) do
-      expect(current_url).to start_with('https://api.twitter.com/oauth/authorize')
-      current_window.close
-    end
-  end
-
-  scenario 'with Facebook' do
-    visit '/'
-
-    click_link 'f Facebook'
-    expect(windows.length).to eq(2)
-
-    within_window(windows.last) do
-      expect(current_url).to start_with('https://www.facebook.com/dialog/oauth')
-      current_window.close
+    scenario 'from settings page' do
+      login_as users(:greg), scope: :user
+      visit '/settings/info'
+      click_link 'Sign Out'
+      page.driver.browser.switch_to.alert.accept
+      expect(current_path).to eq('/')
+      expect(page).to have_content('Sign up or in')
     end
   end
 end

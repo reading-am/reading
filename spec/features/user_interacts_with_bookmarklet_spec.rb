b90e8f3111ebc3f3b3a13a1409256318697a5bd2
Greg Leppert <greg@leppert.me>
2015-04-04 21:45:18 -0400

    Spin yep nope spec out into shared context

--- spec/features/user_interacts_with_bookmarklet_spec.rb
+++ spec/features/user_interacts_with_bookmarklet_spec.rb
@@ -24,18 +24,23 @@ feature "User's bookmarklet", js: true do
     expect(user.posts.reload.count).to eq(db_count + 1), "A post wasn't added to the database"
   end
 
-  scenario 'loads and posts from a site' do
-    visit url
+  shared_context 'a yep nope button' do |yn|
+    scenario "clicking a #{yn} button toggles #{yn} on a post" do
+      visit url
 
-    trigger_bookmarklet_for user
-    post = user.posts.last
-    expect(post.yn).to eq(nil)
+      trigger_bookmarklet_for user
+      post = user.posts.last
+      expect(post.yn).to eq(nil)
 
-    within('#r_am') do
-      click_link('Yep')
-      wait_for_js
+      within('#r_am') do
+        click_link(yn.to_s.capitalize)
+        wait_for_js
+      end
+
+      expect(post.reload.yn).to eq(yn == :yep), "Post's yep nope wasn't updated"
     end
-
-    expect(post.reload.yn).to eq(true), "Post's yep nope wasn't updated"
   end
+
+  it_behaves_like 'a yep nope button', :yep
+  it_behaves_like 'a yep nope button', :nope
 end

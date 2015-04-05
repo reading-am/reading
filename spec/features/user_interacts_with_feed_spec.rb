bd72a597567a1956f8648870c14e8123062e2b4f
Greg Leppert <greg@leppert.me>
2015-04-05 17:29:48 -0400

    Spin out shared feature contexts

--- spec/features/user_interacts_with_feed_spec.rb
+++ spec/features/user_interacts_with_feed_spec.rb
@@ -233,59 +233,13 @@ feature "User's logged in feed", js: true do
         expect(user.posts.count).to eq(db_count - 1), "Post wasn't removed from the database"
       end
 
-      shared_context 'a yep nope button' do |yn|
-        scenario "toggles #{yn} clicking a #{yn} button" do
-          query = user.posts.where(yn: yn.to_s == 'yep')
-          db_count = query.count
-
-          visit url
-          scroll_to_bottom
-
-          row = nil
-          all('.pa_destroy').each do |el|
-            row = first_row_containing(el)
-            if row.all(".pa_#{yn}.r_active").count == 0
-              break
-            else
-              row = nil
-            end
-          end
-
-          expect(row).to be_truthy, "A posted page that didn't already have #{yn} wasn't found"
-
-          within(row) do
-            dom_count = all(".r_subpost .r_#{yn}").count
-
-            find(".pa_#{yn}").click
-            wait_for_js
-            expect(page).to have_selector(".pa_#{yn}.r_active"), "#{yn} button didn't change state"
-            expect(page).to have_selector(".r_subpost .r_#{yn}", count: dom_count + 1), "A post wasn't newly marked as '#{yn}'"
-            expect(query.count).to eq(db_count + 1), "Post wasn't maked as '#{yn}' in the database"
-
-            find(".pa_#{yn}").click
-            wait_for_js
-            expect(page).not_to have_selector(".pa_#{yn}.r_active"), "#{yn} button didn't change state"
-            expect(page).to have_selector(".r_subpost .r_#{yn}", count: dom_count), "'#{yn}' wasn't removed from the subpost DOM"
-            expect(query.count).to eq(db_count), "'#{yn}' wasn't removed from the post in the database"
-          end
-        end
-      end
-
       it_behaves_like 'a yep nope button', :yep
       it_behaves_like 'a yep nope button', :nope
 
-      scenario 'brings up a share sheet clicking a share button' do
-        visit url
-
-        click_link('Share', match: :first)
-        expect(page).to have_selector('#r_share_menu'), "Share menu wasn't displayed"
-
-        click_link('Twitter')
-        expect(windows.length).to eq(2)
-
-        within_window(windows.last) do
-          expect(current_url).to start_with('https://twitter.com/intent')
-          current_window.close
+      it_behaves_like 'is shareable' do
+        let(:share_link) do
+          visit url
+          find_link('Share', match: :first)
         end
       end
     end
@@ -313,6 +267,8 @@ feature "User's logged in feed", js: true do
 
     describe 'comments' do
 
+      let(:comment_el) { first('.r_comment') }
+
       scenario 'input creates a new comment' do
         count = user.comments.count
         body = 'This is a comment'
@@ -334,13 +290,29 @@ feature "User's logged in feed", js: true do
       end
 
       scenario 'delete button deletes a comment' do
+        find('.comments_icon').click
+        comment_el.hover
+        comment_el.click_link('Share')
       end
 
-      scenario 'share button brings up the share sheet' do
+      it_behaves_like 'is shareable' do
+        let(:share_link) do
+          visit url
+          scroll_to_bottom
+          within(first_row_containing('.has_comments')) do
+            find('.comments_icon').click
+            comment = first('.r_comment')
+            comment.hover
+            return comment.find_link('Share')
+          end
+        end
       end
 
       scenario 'permalink goes to the comment page' do
-      end      
+      end
+
+      scenario 'visit user when username is clicked' do
+      end
     end
   end
 end

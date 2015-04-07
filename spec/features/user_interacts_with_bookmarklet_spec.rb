a92117116ea34689d0a462d3ffb022f604a07949
Greg Leppert <greg@leppert.me>
2015-04-06 22:20:37 -0400

    Revise and group bookmarklet specs

--- spec/features/user_interacts_with_bookmarklet_spec.rb
+++ spec/features/user_interacts_with_bookmarklet_spec.rb
@@ -16,56 +16,45 @@ feature "User's bookmarklet", js: true do
     expect(page).to have_selector('.r_comments_with_input'), "UI didn't reflect that a post was created"
   end
 
-  scenario 'loads and posts from a site' do
+  before(:each) do |example|
+    unless example.metadata[:skip_before]
+      visit url
+      trigger_bookmarklet_for user
+    end
+  end
+
+  scenario 'loads and posts from a site', :skip_before do
     db_count = user.posts.count
     visit url
-
     trigger_bookmarklet_for user
     expect(user.posts.reload.count).to eq(db_count + 1), "A post wasn't added to the database"
   end
 
-  shared_context 'a yep nope button' do |yn|
-    scenario "clicking a #{yn} button toggles #{yn} on a post" do
-      visit url
+  describe 'post' do
 
-      trigger_bookmarklet_for user
-      post = user.posts.last
-      expect(post.yn).to eq(nil)
+    shared_context 'a yep nope button' do |yn|
+      scenario "clicking a #{yn} button toggles #{yn} on a post" do
+        post = user.posts.last
+        expect(post.yn).to eq(nil)
 
-      within('#r_am') do
-        click_link(yn.to_s.capitalize)
-        wait_for_js
+        within('#r_am') do
+          click_link(yn.to_s.capitalize)
+          wait_for_js
+        end
+
+        expect(post.reload.yn).to eq(yn == :yep), "Post's yep nope wasn't updated"
       end
-
-      expect(post.reload.yn).to eq(yn == :yep), "Post's yep nope wasn't updated"
     end
-  end
 
-  it_behaves_like 'a yep nope button', :yep
-  it_behaves_like 'a yep nope button', :nope
+    it_behaves_like 'a yep nope button', :yep
+    it_behaves_like 'a yep nope button', :nope
 
-  scenario "clicking a share button brings up a share sheet" do
-    visit url
-
-    trigger_bookmarklet_for user
-    click_link('Share', match: :first)
-    expect(page).to have_selector('#r_share_menu'), "Share menu wasn't displayed"
-
-    click_link('Twitter')
-    expect(windows.length).to eq(2)
-
-    within_window(windows.last) do
-      expect(current_url).to start_with('https://twitter.com/intent')
-      current_window.close
+    it_behaves_like 'is shareable' do
+      let(:share_link) { find_link('Share', match: :first) }
     end
   end
 
   describe 'subposts' do
-
-    before(:each) do
-      visit url
-      trigger_bookmarklet_for user
-    end
 
     scenario 'are rendered on initial load' do
       expect(page).to have_selector('#r_readers')
@@ -78,11 +67,6 @@ feature "User's bookmarklet", js: true do
   end
 
   describe 'comments' do
-
-    before(:each) do
-      visit url
-      trigger_bookmarklet_for user
-    end
 
     it_behaves_like 'comment input'
 

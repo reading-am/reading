72a11fd24e5680c2345569faf6beac1a816ae85b
Greg Leppert <greg@leppert.me>
2015-05-31 15:21:45 -0400

    Add render_api to mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
+  include RenderApi
   default from: "Reading <mailman@#{ROOT_DOMAIN}>"
 
   ## HELPERS
   # these used to be in a separate file but they were being
   # included with the normal app controller
-  helper_method :current_user, :signed_in?, :user_signed_in?
+  helper_method :current_user,
+                :signed_in?,
+                :user_signed_in?,
+                :render_api
 
   def current_user
     @current_user ||= User.new

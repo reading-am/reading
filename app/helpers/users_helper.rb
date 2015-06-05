b5023463a7ff60387f3b460f725735a12edf34c1
Greg Leppert <greg@leppert.me>
2015-06-05 11:34:43 -0400

    Better handle blank slate in JS posts rendering
    This needs to be reworked entirely, but this should prevent people from being unable to see their own posts when they don't follow anyone

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -11,17 +11,16 @@ module UsersHelper
     if @user
       user = render_api partial: 'users/user', user: @user
       has_avatar = @user != current_user or !@user.avatar.size.nil?
-      user.merge!({
-        'show_blank_slate' =>   (@user.posts.size == 0 or @user.following.size == 0),
-        'is_current_user' =>    @user == current_user,
-        'avatar_link_url' =>    has_avatar ? @user.avatar_url : "/settings/info",
-        'avatar_link_target' => has_avatar ? "_blank" : false,
-        'you_or_name' =>        @user == current_user ? 'you' : @user.first_name,
-        'is_following' =>       signed_in? && current_user.following?(@user),
-        'has_followed' =>       @user.following.size > 0,
-        'has_link' =>           !@user.link.blank?,
-        'has_location' =>       !@user.location.blank?
-      })
+      user.merge!({'show_blank_slate' =>   ((params[:type] == 'posts' && @user.posts.size == 0) ||
+                                            (params[:type] == 'list' && @user.following.size == 0)),
+                   'is_current_user' =>    @user == current_user,
+                   'avatar_link_url' =>    has_avatar ? @user.avatar_url : "/settings/info",
+                   'avatar_link_target' => has_avatar ? "_blank" : false,
+                   'you_or_name' =>        @user == current_user ? 'you' : @user.first_name,
+                   'is_following' =>       signed_in? && current_user.following?(@user),
+                   'has_followed' =>       @user.following.size > 0,
+                   'has_link' =>           !@user.link.blank?,
+                   'has_location' =>       !@user.location.blank?})
       data[:user] = user
     end
 

6f24cc2dc7decb9529fa4c11f5b5e216da4bd7ee
Greg Leppert <greg@leppert.me>
2015-09-05 17:34:07 -0400

    Tweak puma config per heroku example

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,17 @@
-preload_app!
- 
-min_threads = Integer(ENV['MIN_THREADS'] || 8)
-max_threads = Integer(ENV['MAX_THREADS'] || 12)
- 
-threads min_threads, max_threads
+# via: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#config
+
 workers Integer(ENV['WORKER_COUNT'] || 2)
+
+min_threads = Integer(ENV['MIN_THREADS'] || 5)
+max_threads = Integer(ENV['MAX_THREADS'] || 10)
+threads min_threads, max_threads
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end

bc6630399c0dcdcbbfb48ceb07859291d00eb8d9
Greg Leppert <greg@leppert.me>
2015-09-05 15:23:06 -0400

    Clean up user observer logic

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,31 +1,44 @@
 class UserObserver < ActiveRecord::Observer
 
-  def before_save user
-    # when it's a new user and they have a username
-    if (user.username_was.blank? or user.email_was.blank?) and !user.username.blank? and !user.email.blank?
+  def before_save(user)
+    return unless new_user?(user)
 
-      PusherJob.perform_later 'create', user
+    PusherJob.perform_later 'create', user
+    UserMailer.welcome(user).deliver_later
 
-      # Tweet to ReadingArrivals
-      if !user.joined_before_email? and Rails.env == 'production'
-        tweet = "Everyone welcome #{user.username}! #{ROOT_URL}/#{user.username}"
-        TweetJob.new.async.perform ENV['READING_ARRIVALS_TOKEN'], ENV['READING_ARRIVALS_SECRET'], tweet
-      end
-
-      UserMailer.welcome(user).deliver_later unless user.joined_before_email?
-
-    end
+    # Tweet to ReadingArrivals
+    return unless Rails.env.production?
+    tweet = "Everyone welcome #{user.username}! #{ROOT_URL}/#{user.username}"
+    TweetJob.new.async.perform ENV['READING_ARRIVALS_TOKEN'],
+                               ENV['READING_ARRIVALS_SECRET'],
+                               tweet
   end
 
-  def after_update user
+  def after_update(user)
     PusherJob.perform_later 'update', user
   end
 
-  def after_destroy user
+  def after_destroy(user)
     PusherJob.perform_later 'destroy', user
 
-    # This email can't be delayed because the user won't exist by the time it's processed
-    UserMailer.destroyed(user).deliver_now unless user.email.blank? or user.username.blank?
+    # Don't perform any more callbacks if this was an aborted registration
+    return if aborted_registration?(user)
+
+    # This can't be delayed because user won't exist by the time it's processed
+    UserMailer.destroyed(user).deliver_now
   end
 
+  private
+
+  def new_user?(user)
+    (
+      user.username_was.blank? ||
+      (user.email_was.blank? && !user.joined_before_email?)
+    ) &&
+      user.username.present? && user.email.present?
+  end
+
+  def aborted_registration?(user)
+    user.email.blank? || user.username.blank?
+  end
 end

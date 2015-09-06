9ace4740f946c74500448945b564723736ae612a
Greg Leppert <greg@leppert.me>
2015-09-06 13:23:20 -0400

    Cleanup

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,11 +1,11 @@
 Paperclip::Attachment.default_options.merge!(
-  :storage => :s3,
-  :s3_protocol => 'https',
-  :bucket => ENV['S3_BUCKET'],
-  :s3_host_alias => ENV['S3_BUCKET'],
-  :s3_credentials => {
-    :access_key_id => ENV['S3_KEY'],
-    :secret_access_key => ENV['S3_SECRET']
+  storage: :s3,
+  s3_protocol: 'https',
+  bucket: ENV['S3_BUCKET'],
+  s3_host_alias: ENV['S3_BUCKET'],
+  s3_credentials: {
+    access_key_id: ENV['S3_KEY'],
+    secret_access_key: ENV['S3_SECRET']
   }
 )
 

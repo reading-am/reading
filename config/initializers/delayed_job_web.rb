e60ad15dde0ecd67996084326165152507e2c55c
Greg Leppert <greg@leppert.me>
2015-11-01 15:05:26 -0500

    Remove delayed_job relics

--- config/initializers/delayed_job_web.rb
+++
@@ -1,4 +0,0 @@
-# for use with: https://github.com/ejschmitt/delayed_job_web
-DelayedJobWeb.use Rack::Auth::Basic do |username, password|
-  username == ENV['AUTH_BASIC_USER'] && password == ENV['AUTH_BASIC_PASS']
-end

c7569ff8984a5b6fa0e8b401f7275fe6644ad6a2
Greg Leppert <greg@leppert.me>
2015-11-02 22:36:33 -0500

    Return the hook result for testing

--- app/models/hook.rb
+++ app/models/hook.rb
@@ -65,6 +65,7 @@ public
   def run(post, event_fired)
     return unless responds_to event_fired
     HookJob.perform_later(self, post, event_fired.to_s)
+    send(provider, post, event_fired) if Rails.env.test?
   end
 
   def facebook(post, event_fired)

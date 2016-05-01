b35f7acff915dd24330bd6db60e3c1ddbc0dba36
Greg Leppert <greg@leppert.me>
2016-05-01 19:34:20 -0400

    Add case and whitespace insensitive query

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,4 +7,18 @@ class Tag < ActiveRecord::Base
   validates :page_id, presence: true
 
   strip_attributes only: [:name]
+
+  class << self
+    def find_by(*args)
+      return super(*args) unless Hash === args.first && args.first.key?(:name)
+      new_args = args.first.dup
+      name = new_args.delete(:name)
+      where_name(name).where(new_args).take
+    end
+
+    def where_name(name)
+      formatter = lambda { |c| "lower(trim(regexp_replace(#{c}, '\s+', '', 'g')))" }
+      where("#{formatter.call('name')} = #{formatter.call('?')}", name)
+    end
+  end
 end

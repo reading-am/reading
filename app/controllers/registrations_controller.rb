9dc54091a5720cbf9d4a1da65c2ea4c8ee41e311
Greg Leppert <greg@leppert.me>
2016-07-30 12:12:51 -0400

    Replace deprecated bypass option

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -57,7 +57,7 @@ class RegistrationsController < Devise::RegistrationsController
           :update_needs_confirmation : :updated
         set_flash_message :notice, flash_key
       end
-      sign_in resource_name, resource, :bypass => true
+      bypass_sign_in resource_name, resource
       respond_with resource, :location => after_update_path_for(resource)
     else
       clean_up_passwords resource
@@ -79,7 +79,7 @@ class RegistrationsController < Devise::RegistrationsController
 
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     if resource.send("update_with#{change_requires_password ? '' : 'out'}_password", account_update_params)
-      sign_in resource_name, resource, bypass: true
+      bypass_sign_in resource_name, resource
       redirect_to("/#{resource.username}/list", notice: 'User was successfully updated.')
     else
       clean_up_passwords resource

e128bbc0b487207290550353bbba9cebbb035aee
Greg Leppert <greg@leppert.me>
2016-07-31 15:59:24 -0400

    Remove rspec-active job gem
    This functionality is now in rspec-rails core

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,16 +13,14 @@ describe Post do
       user = users(:greg)
       expect(user.hooks.count).to be > 1
       post = Post.create user: user, page: pages(:daringfireball)
-      expect(HookJob).to have_been_enqueued
-                          .times(user.hooks.count)
-                          .with(global_id(user.hooks.first),
-                                global_id(post),
-                                'new')
+      user.hooks.each do |hook|
+        expect(HookJob).to have_been_enqueued.once.with(hook, post, 'new')
+      end
     end
 
     it "sends a pusher notice", commit: true do
       post = Post.create user: users(:greg), page: pages(:daringfireball)
-      expect(PusherJob).to have_been_enqueued.once.with('create', global_id(post))
+      expect(PusherJob).to have_been_enqueued.once.with('create', post)
     end
   end
 
@@ -33,17 +31,15 @@ describe Post do
       hooks = user.hooks.select { |hook| hook.responds_to(:yep) }
 
       post.update yn: true
-      expect(HookJob).to have_been_enqueued
-                          .times(hooks.length)
-                          .with(global_id(hooks.first),
-                                global_id(post),
-                                'yep')
+      hooks.each do |hook|
+        expect(HookJob).to have_been_enqueued.once.with(hook, post, 'yep')
+      end
     end
 
     it "sends a pusher notice", commit: true do
       post = posts(:one)
       post.update yn: !post.yn
-      expect(PusherJob).to have_been_enqueued.once.with('update', global_id(post))
+      expect(PusherJob).to have_been_enqueued.once.with('update', post)
     end
   end
 
@@ -51,7 +47,7 @@ describe Post do
     it "sends a pusher notice", commit: true do
       post = posts(:one)
       post.destroy
-      expect(PusherJob).to have_been_enqueued.once.with('destroy', global_id(post))
+      expect(PusherJob).to have_been_enqueued.once.with('destroy', post)
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,12 +11,12 @@ describe User do
   context "when created", commit: true do
     it "sends a pusher notice", commit: true do
       user = User.create username: 'tester', email: 'greg@example.com', password: 'thisisatestpass'
-      expect(PusherJob).to have_been_enqueued.once.with('create', global_id(user))
+      expect(PusherJob).to have_been_enqueued.once.with('create', user)
     end
 
     it "sends a welcome email", commit: true do
       user = User.create username: 'tester', email: 'greg@example.com', password: 'thisisatestpass'
-      expect(ActionMailer::DeliveryJob).to have_been_enqueued.once.with('UserMailer', 'welcome', 'deliver_now', global_id(user))
+      expect(ActionMailer::DeliveryJob).to have_been_enqueued.once.with('UserMailer', 'welcome', 'deliver_now', user)
     end
   end
 
@@ -24,14 +24,14 @@ describe User do
     it "sends a pusher notice", commit: true do
       user = users(:greg)
       user.update username: 'a_new_username'
-      expect(PusherJob).to have_been_enqueued.once.with('update', global_id(user))
+      expect(PusherJob).to have_been_enqueued.once.with('update', user)
     end
 
     it "sends a welcome email if username or email was previously missing", commit: true do
       user = User.create username: 'tester', password: 'thisisatestpass'
       expect(ActionMailer::DeliveryJob).to_not have_been_enqueued
       user.update email: 'greg@example.com'
-      expect(ActionMailer::DeliveryJob).to have_been_enqueued.once.with('UserMailer', 'welcome', 'deliver_now', global_id(user))
+      expect(ActionMailer::DeliveryJob).to have_been_enqueued.once.with('UserMailer', 'welcome', 'deliver_now', user)
     end
   end
 
@@ -39,7 +39,7 @@ describe User do
     it "sends a pusher notice" do
       user = users(:greg)
       user.destroy
-      expect(PusherJob).to have_been_enqueued.once.with('destroy', global_id(user))
+      expect(PusherJob).to have_been_enqueued.once.with('destroy', user)
     end
   end
 end
